6ec3400a81d3041b7103b5556fb0be1c7ad563cd
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-11-22 05:43:35 +1100

    Add files via upload

--- preprocessor.c
+++ preprocessor.c
@@ -5,18 +5,24 @@
 #include <stdio.h>
 #include <locale.h>
 
+#define collection switch
 #define then {
+#define target case
 #define end }
+#define jmp goto
 
 FILE *desc[2] = {NULL, NULL};
-unsigned char container[0xFFFF];
+unsigned char container[0xFFFF] = {'\0'};
 /**
 unsigned char container_1[0xFFFF];
 unsigned char container_2[0xFFFF];
 /**/
 
-unsigned long long row = 0; // строка
-unsigned long long column = 0; // столбец
+long long iter_0 = 0;
+
+unsigned long long row = 1; // строка
+unsigned long long column = 1; // столбец / позиция
+unsigned long long position = 1; // позиция / индекс/элемент
 
 int main(int argc, char *argv[]) then
  setlocale(0, "");
@@ -80,8 +86,27 @@ _2: fprintf(df, "\n-");
  desc[1] = fopen("preprocessor/source_code.ca", "wb");
  if (desc[1] == NULL) { printf("\nDebug: code = -3"); return -3; }
 
- /// /// ///
-
+ putchar('\n');
+ iter_0 = -1;
+_2_1:
+ collection (container[++iter_0]) then
+ target '\0':
+  jmp _2_2;
+ target '\n':
+  ++position;
+  ++row;
+  column = 1; 
+  jmp _2_1;
+ target '/':
+  printf("[preprocessor - строка: %d, столбец: %d, позиция: %d]: Возможно начало комментария?\n", row, column, position);
+  ++position;
+  ++column;
+  jmp _2_1;
+ end
+ ++position;
+ ++column;
+ jmp _2_1;
+_2_2:
  fclose(desc[1]);
  return 0;
 end
