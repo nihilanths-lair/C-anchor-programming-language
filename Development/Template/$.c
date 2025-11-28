909d4e589583bf48dd918579b4851a4c9b58c86d
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-11-28 11:33:11 +1100

    Add files via upload

--- Development/Template/$.c
+++ Development/Template/$.c
@@ -1,95 +1,112 @@
 #include <stdio.h>
 #include <locale.h>
-/************************************************************/
-/*                       DECLARATION                        */
-/************************************************************/
-/*1*/void print_code_2d_arr(const char *arr[]);
-/************************************************************/
-/*                         STORAGE                          */
-/************************************************************/
-// Подаваемый код на вход лексеру
-//char *code = NULL;
+
+//----------------------------------------------------------//
+//                         STORAGE                          //
+//----------------------------------------------------------//
 char code[0xFFF];
-// Нотация регулярной грамматики (тип 4) для лексера
-// Загрузку производим напрямую с файла
-//char *regular_grammar_notation;
+// РќРѕС‚Р°С†РёСЏ СЂРµРіСѓР»СЏСЂРЅРѕР№ РіСЂР°РјРјР°С‚РёРєРё (С‚РёРї 4) РґР»СЏ Р»РµРєСЃРµСЂР°
 char regular_grammar_notation[0xFFF];
-// Нотация регулярной грамматики (тип 4) для лексера
+typedef struct {
+    const char *input; // РІС…РѕРґРЅРѕР№ РїРѕС‚РѕРє
+    const char *cursor;
+    int line; // РїРѕР·РёС†РёСЏ РІ СЃС‚СЂРѕРєРµ
+    int column; // РїРѕР·РёС†РёСЏ РІ СЃС‚РѕР»Р±С†Рµ (РєРѕР»РѕРЅРєРµ)
+} Lexer;
 typedef enum {
-    TOKEN__NUMBER,
-    TOKEN__VARIABLE,
-    TOKEN__SEMICOLON,
-    TOKEN__SPACE,
-    TOKEN__ASSIGN,
-    TOKEN__LOG,
-    TOKEN__PLUS,
-    TOKEN__MINUS,
-    TOKEN__LPAR,
-    TOKEN__RPAR
-} Type;
-// Класс: Тип токена
-typedef struct {
-    Type name;         // Тип токена
-    const char *regex; // Регулярное выражение типа токена
+    TOKEN_NUMBER,
+    TOKEN_IDENT,
+    TOKEN_EOF
 } TokenType;
-// Класс: Токен
 typedef struct {
-    int number;         // Порядковый номер токена
-    TokenType type;     // Тип токена
-    const char *begin;  // Начало токена
-    const char *end;    // Конец токена
-    int pos_line;       // Позиция токена в строке
-    int pos_column;     // Позиция токена в столбце
+    TokenType type;
+    const char *begin;
+    const char *end;
+    int line;
+    int column;
 } Token;
-/**/
 FILE *file = NULL;
 size_t symbols;
-/************************************************************/
-/*                        PROGRAM                           */
-/************************************************************/
+//----------------------------------------------------------//
+//
+//
+//
+//----------------------------------------------------------//
+//                       DECLARATION                        //
+//----------------------------------------------------------//
+void lexer_init(Lexer *lexer, const char *code);
+Token lexer_nextToken(Lexer *lexer);
+//----------------------------------------------------------//
+//----------------------------------------------------------//
+//                        PROGRAM                           //
+//----------------------------------------------------------//
 int main()
 {
     setlocale(0, "");
 
     file = fopen("$.bnf", "rb");
     if (file == NULL) { printf("\nAn exception was thrown: 1."); return 1; }
-    /** Временно убрано, пока без malloc, задано фиксировано
+    /** Р’СЂРµРјРµРЅРЅРѕ СѓР±СЂР°РЅРѕ, РїРѕРєР° Р±РµР· malloc, Р·Р°РґР°РЅРѕ С„РёРєСЃРёСЂРѕРІР°РЅРѕ
     fseek(file, 0, SEEK_END);
     long flength = ftell(file);
-    fseek(file, 0, SEEK_SET); IGNORE: Имеет ли смысл это делать сразу, или лучше потом, когда понадобится/потребуется?
+    fseek(file, 0, SEEK_SET); IGNORE: РРјРµРµС‚ Р»Рё СЃРјС‹СЃР» СЌС‚Рѕ РґРµР»Р°С‚СЊ СЃСЂР°Р·Сѓ, РёР»Рё Р»СѓС‡С€Рµ РїРѕС‚РѕРј, РєРѕРіРґР° РїРѕРЅР°РґРѕР±РёС‚СЃСЏ/РїРѕС‚СЂРµР±СѓРµС‚СЃСЏ?
     */
-    printf("\nДо: regular_grammar_notation = \"%s\".", regular_grammar_notation); // До загрузки грамматики
+    printf("\nР”Рѕ: regular_grammar_notation = \"%s\".", regular_grammar_notation); // Р”Рѕ Р·Р°РіСЂСѓР·РєРё РіСЂР°РјРјР°С‚РёРєРё
     symbols = fread(regular_grammar_notation, 1, sizeof (regular_grammar_notation) - 1, file);
     fclose(file);
     regular_grammar_notation[symbols] = '\0';
     printf("\nsymbols = %lld.", symbols);
-    printf("\nПосле: regular_grammar_notation = \"\n%s\n\".", regular_grammar_notation); // После загрузки грамматики
+    printf("\nРџРѕСЃР»Рµ: regular_grammar_notation = \"\n%s\n\".", regular_grammar_notation); // РџРѕСЃР»Рµ Р·Р°РіСЂСѓР·РєРё РіСЂР°РјРјР°С‚РёРєРё
 
     file = fopen("$.txt", "rb");
     if (file == NULL) { printf("\nAn exception was thrown: 2."); return 2; }
-    /** Временно убрано, пока без malloc, задано фиксировано
+    /** Р’СЂРµРјРµРЅРЅРѕ СѓР±СЂР°РЅРѕ, РїРѕРєР° Р±РµР· malloc, Р·Р°РґР°РЅРѕ С„РёРєСЃРёСЂРѕРІР°РЅРѕ
     fseek(file, 0, SEEK_END);
     long flength = ftell(file);
-    fseek(file, 0, SEEK_SET); // IGNORE: Имеет ли смысл это делать сразу, или лучше потом, когда понадобится/потребуется?
+    fseek(file, 0, SEEK_SET); // IGNORE: РРјРµРµС‚ Р»Рё СЃРјС‹СЃР» СЌС‚Рѕ РґРµР»Р°С‚СЊ СЃСЂР°Р·Сѓ, РёР»Рё Р»СѓС‡С€Рµ РїРѕС‚РѕРј, РєРѕРіРґР° РїРѕРЅР°РґРѕР±РёС‚СЃСЏ/РїРѕС‚СЂРµР±СѓРµС‚СЃСЏ?
     */
-    printf("\nДо: code = \"%s\".", code); // До загрузки кода
+    printf("\nР”Рѕ: code = \"%s\".", code); // Р”Рѕ Р·Р°РіСЂСѓР·РєРё РєРѕРґР°
     symbols = fread(code, 1, sizeof (code) - 1, file);
     fclose(file);
     code[symbols] = '\0';
     printf("\nsymbols = %lld.", symbols);
-    printf("\nПосле: code = \"\n%s\n\".", code); // После загрузки кода
-
-    const char *ptr__code = code;
-    while (*ptr__code != '\0')
+    printf("\nРџРѕСЃР»Рµ: code = \"\n%s\n\".", code); // РџРѕСЃР»Рµ Р·Р°РіСЂСѓР·РєРё РєРѕРґР°
+    
+    putchar('\n');
+    Lexer lexer;
+    lexer_init(&lexer, code);
+    for (;;)
     {
-        //распознаём токен
-        //cursor += длина токена
-        ptr__code++;
+        Token token = lexer_nextToken(&lexer);
+        //print_token(token);
+        printf("token.type = %d", token.type);
+        if (token.type == TOKEN_EOF) break;
     }
     putchar('\n');
     return 0;
 }
-/************************************************************/
-/*                      SUBPROGRAM                          */
-/************************************************************/
-/*1*/void print_code_2d_arr(const char *arr[]) { for (int i = 0; arr[i] != NULL; i++) printf("%s", arr[i]); }
+//----------------------------------------------------------//
+//
+//
+//
+//----------------------------------------------------------//
+//                      SUBPROGRAM                          //
+//----------------------------------------------------------//
+void lexer_init(Lexer *lexer, const char *code)
+{
+    lexer->input = code;
+    lexer->cursor = code;
+    lexer->line = 0;
+    lexer->column = 0;
+}
+// Р—Р°РіР»СѓС€РєР°
+Token lexer_nextToken(Lexer *lexer)
+{
+    Token token;
+    token.type = TOKEN_EOF;
+    token.begin = token.end = lexer->cursor;
+    token.line = lexer->line;
+    token.column = lexer->column;
+    return token;
+}
+//----------------------------------------------------------//
