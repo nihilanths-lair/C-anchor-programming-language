c7a3bc25c781932e7d22586d29f0ecc8b8b7f51c
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-12-03 02:12:51 +1100

    Add files via upload

---
+++ peg/build.bat
@@ -0,0 +1,5 @@
+@echo off
+gcc main.c peg_ast.c -o peg_app
+echo Build complete.
+echo Starting the build.
+peg_app
---
+++ peg/peg_ast.c
@@ -0,0 +1,14 @@
+#include "peg_ast.h"
+
+PNode* pnode_new(enum PNodeType type, const char *text)
+{
+
+}
+void pnode_add(PNode *parent, PNode *child)
+{
+
+}
+void pnode_free(PNode *node)
+{
+
+}
