d9bd9ca85a0730f7539eae5dde858e4081a109c3
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-12-11 21:04:41 +1100

    Delete LANGUAGE_TOOL/LANGUAGE_TOOL/SyntacticAnalyzer.c

--- LANGUAGE_TOOL/LANGUAGE_TOOL/SyntacticAnalyzer.c
+++
@@ -1,6 +0,0 @@
-// #2 Синтаксический анализатор (Парсер) / Syntactic analyzer (Parser)
-#include "SyntacticAnalyzer.h"
-
-unsigned char syntactic_analyzer_pattern[LIMIT_SIZE_SYNTACTIC_ANALYZER];
-
-// ... //
