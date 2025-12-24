c19cc66f6dc78745e6d065f0b2ea0cdfb5f0dd82
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-12-24 13:38:33 +1100

    Add files via upload

--- LANGUAGE_TOOL/prim.c
+++ LANGUAGE_TOOL/prim.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #define _ int main
+int i;
 _()
 {
     printf("\nswitch-case");
-    switch (3){
-    case 1: printf("\n1");
-    case 2: printf("\n2");
-    case 3: printf("\n3");
-    case 4: printf("\n4");
-    case 5: printf("\n5");
+    i = 3;
+    switch (i){
+    case 1: printf("\n1"); // break;
+    case 2: printf("\n2"); // break;
+    case 3: printf("\n3"); // break;
+    case 4: printf("\n4"); // break;
+    default: printf("\n0 || 5 .. max");
     }
     
     printf("\n\nwhile");
-    int i = 3;
-    while (i <= 5){
+    i = 3;
+    while (i <= 4){
         printf("\n%d", i);
         i++;
     }
+    printf("\n0 || 5 .. max");
 
     printf("\n\ncomputed goto");
-    void * addr[] = {&&case_1, &&case_2, &&case_3, &&case_4, &&case_5};
-    goto * addr[3-1];
-    case_1: printf("\n1");
-    case_2: printf("\n2");
-    case_3: printf("\n3");
-    case_4: printf("\n4");
-    case_5: printf("\n5");
+    void * addr[] = {&&case_1, &&case_2, &&case_3, &&case_4};
+    i = 3-1;
+    if (i > 0 && i < 5) goto * addr[i];
+    else goto default_;
+    case_1: printf("\n1"); // goto forward_;
+    case_2: printf("\n2"); // goto forward_;
+    case_3: printf("\n3"); // goto forward_;
+    case_4: printf("\n4"); // goto forward_;
+    default_: printf("\n0 || 5 .. max"); // goto forward_;
+    forward_:
 
     putchar('\n');
 }
