af63eac0f1c0d74d10475b85c6b329cae9f4b1c7
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-12-25 14:17:43 +1100

    Add files via upload

--- Language_workbench/1/_.c
+++ Language_workbench/1/_.c
@@ -9,32 +9,14 @@ int main()
 {
    setlocale(0, "");
 
-   parse_analysis("code...");
-   //synthesis();
-   //parse("code...");
-   //compile("code...");
-   //execute();
+   lexical_analyzer("/!\\");
+   lexical_synthesizer("/!\\");
+
+   syntactic_analyzer("/!\\");
+   syntactic_synthesizer("/!\\");
+
+   semantic_analyzer("/!\\");
+   semantic_synthesizer("/!\\");
 
    return 0;
-}
-
-void compile(const char *code)
-{
-   //parse(code);
-}
-void execute(const char *code)
-{
-   //parse(code);
-}
-void parse(const char *code)
-{
-   //analysis();
-   //synthesis();
-}
-void parse_analysis(const char *code)
-{
-   //synthesis();
-}
-void parse_synthesis(const char *code)
-{
 }
--- Language_workbench/1/_.h
+++ Language_workbench/1/_.h
@@ -1,15 +1,27 @@
 /** [Path: Language_workbench/1/_.h] **/
 
-/*/ |№:1-1| /*/ void compile(const char *code);
-/*/ |№:1-2| /*/ void execute(const char *code);
+/*/ |№:1-1| /*/ //void compile(const char *code);
+/*/ |№:1-2| /*/ //void execute(const char *code);
 
 // Lexer.h (опционально)
-/*/ |№:2-1| {?} /*/
+/*/ |№:2-1| /*/ void lexical_analyzer(const char *data);
+/*/ |№:2-2| /*/ void lexical_synthesizer(const char *data);
+
+void lexical_analyzer(const char *data) { return; }
+void lexical_synthesizer(const char *data) { return; }
 
 // Parser.h //
-/*/ |№:3-1| /*/ void parse(const char *code);
-/*/ |№:3-2| /*/ void parse_analysis(const char *code);
-/*/ |№:3-3| /*/ void parse_synthesis(const char *code);
+/*/ |№:3-1| /*/ //void parse(const char *code);
+
+/*/ |№:3-2| /*/ void syntactic_analyzer(const char *data);
+/*/ |№:3-3| /*/ void syntactic_synthesizer(const char *data);
+
+void syntactic_analyzer(const char *data) { return; }
+void syntactic_synthesizer(const char *data) { return; }
 
 // Semantic.h //
-/*/ |№:4-1| {?} /*/
+/*/ |№:4-1| /*/ void semantic_analyzer(const char *data);
+/*/ |№:4-2| /*/ void semantic_synthesizer(const char *data);
+
+void semantic_analyzer(const char *data) { return; }
+void semantic_synthesizer(const char *data) { return; }
