d92d520b0281d2e48748664bba7dbff1962eb7f4
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-12-25 13:46:01 +1100

    Add files via upload

--- Language_workbench/1/_.h
+++ Language_workbench/1/_.h
@@ -1,9 +1,12 @@
 /** [Path: Language_workbench/1/_.h] **/
 
-/*/ |№1| /*/ void compile(const char *code);
-/*/ |№2| /*/ void execute(const char *code);
+/*/ |№:1-1| /*/ void compile(const char *code);
+/*/ |№:1-2| /*/ void execute(const char *code);
 
-// Parser //
-/*/ |№3| /*/ void parse(const char *code);
-/*/ |№4| /*/ void parse_analysis(const char *code);
-/*/ |№5| /*/ void parse_synthesis(const char *code);
+// Lexer.h (опционально)
+/*/ |№:2-1| /*/
+
+// Parser.h //
+/*/ |№:3-1| /*/ void parse(const char *code);
+/*/ |№:3-2| /*/ void parse_analysis(const char *code);
+/*/ |№:3-3| /*/ void parse_synthesis(const char *code);
