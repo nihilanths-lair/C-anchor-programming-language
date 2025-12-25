988ed282a7d6cf56cd6b16092deaf4881085c093
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-12-25 14:22:25 +1100

    Add files via upload

--- Language_workbench/1/_.h
+++ Language_workbench/1/_.h
@@ -3,7 +3,7 @@
 /*/ |№:1-1| /*/ //void compile(const char *code);
 /*/ |№:1-2| /*/ //void execute(const char *code);
 
-// Lexer.h (опционально)
+// Tokenizer.h (опционально)
 /*/ |№:2-1| /*/ void lexical_analyzer(const char *data);
 /*/ |№:2-2| /*/ void lexical_synthesizer(const char *data);
 
@@ -11,13 +11,13 @@ void lexical_analyzer(const char *data) { return; }
 void lexical_synthesizer(const char *data) { return; }
 
 // Parser.h //
-/*/ |№:3-1| /*/ //void parse(const char *code);
-
-/*/ |№:3-2| /*/ void syntactic_analyzer(const char *data);
-/*/ |№:3-3| /*/ void syntactic_synthesizer(const char *data);
+/*/ |№:3-1| /*/ void syntactic_analyzer(const char *data);
+/*/ |№:3-2| /*/ void syntactic_synthesizer(const char *data);
 
 void syntactic_analyzer(const char *data) { return; }
 void syntactic_synthesizer(const char *data) { return; }
+
+/*/ |№:3-3| /*/ //void parse(const char *code);
 
 // Semantic.h //
 /*/ |№:4-1| /*/ void semantic_analyzer(const char *data);
