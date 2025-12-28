7137e827aed9acc6719a52bb92cc83d729d6772b
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-12-29 09:00:35 +1100

    Add files via upload

---
+++ Language_workbench/1/site/index.php
@@ -0,0 +1,119 @@
+<?php
+  $file_name_1 = "Interpreter_C\$_v-0.x_";
+  $file_name_2 = "VM_C\$_v-0.x_";
+  $file_name_3 = "Compiler_C\$_v-0.x_";
+  $txt = "txt";
+  $exe = "exe";
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+  <title>Главная</title>
+  <style>
+  h4 {
+    font-family: "Courier New", "monospace";
+    font-size: 13px;
+  }
+  </style>
+</head>
+<body style="background-color: #F0F0F0;">
+  <center>
+    <h3>
+      Дата создания сайта: 28/12/2025 | Последнее обновление: 29/12/2025<br>
+      /!\ Убедительная просьба дождаться окончания реконструкции сайта! Окончательный запуск сайта будет произведён в начале января 2026 года.
+    </h3>
+    <hr>
+    Операционная система: Windows 10+<br>
+    <hr><br>
+    <div>
+      Программное обеспечение: <a href="https://ru.wikipedia.org/wiki/%D0%98%D0%BD%D1%82%D0%B5%D1%80%D0%BF%D1%80%D0%B5%D1%82%D0%B0%D1%82%D0%BE%D1%80" target="blank">Интерпретатор</a> C$<br>
+      Последние изменения в ветке: 28/12/2025
+    </div>
+    <br><br>
+    <h4>
+      Имя файла: <?=$file_name_1.'.'.$exe?><br>
+      Версия: 0.X (Beta)<br>
+      Дата публикации: XX/XX/202X<br>
+      Размер файла: 0 байт
+    </h4>
+    <h4>
+      <a href=<?=$file_name_1.'.'.$txt?>><button>Содержимое файла</button></a>&nbsp;&nbsp;::&nbsp;&nbsp;<a href=<?=$file_name_1.'.'.$exe?> download><button>Скачать</button></a>
+    </h4>
+    <br>
+    <h4>
+      Имя файла: <?=$file_name_1.'.'.$exe?><br>
+      Версия: 0.X (Release)<br>
+      Дата публикации: XX/XX/202X<br>
+      Размер файла: 0 байт
+    </h4>
+    <h4>
+      <a href=<?=$file_name_1.'.'.$txt?>"><button>Содержимое файла</button></a>&nbsp;&nbsp;::&nbsp;&nbsp;<a href=<?=$file_name_1.'.'.$exe?> download><button>Скачать</button></a>
+    </h4>
+    <br><hr><br>
+    <div>
+      Программное обеспечение: <a href="https://ru.wikipedia.org/wiki/%D0%92%D0%B8%D1%80%D1%82%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F_%D0%BC%D0%B0%D1%88%D0%B8%D0%BD%D0%B0" target="blank">Виртуальная машина</a> C$<br>
+      Последние изменения в ветке: 28/12/2025
+    </div>
+    <br><br>
+    <h4>
+      Имя файла: <?=$file_name_2.'.'.$exe?><br>
+      Версия: 0.X (Beta)<br>
+      Дата публикации: XX/XX/202X<br>
+      Размер файла: 0 байт
+    </h4>
+    <h4>
+      <a href=<?=$file_name_2.'.'.$txt?> target="blank"><button>Содержимое файла</button></a>&nbsp;&nbsp;::&nbsp;&nbsp;<a href=<?=$file_name_2.'.'.$exe?> download><button>Скачать</button></a>
+    </h4>
+    <br>
+    <h4>
+      Имя файла: <?=$file_name_2.'.'.$exe?><br>
+      Версия: 0.X (Release)<br>
+      Дата публикации: XX/XX/202X<br>
+      Размер файла: 0 байт
+    </h4>
+    <h4>
+      <a href=<?=$file_name_2.'.'.$txt?> target="blank"><button>Содержимое файла</button></a>&nbsp;&nbsp;::&nbsp;&nbsp;<a href=<?=$file_name_2.'.'.$exe?> download><button>Скачать</button></a>
+    </h4>
+    <br><hr><br>
+    <div>
+      Программное обеспечение: <a href="https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D0%BC%D0%BF%D0%B8%D0%BB%D1%8F%D1%82%D0%BE%D1%80" target="blank">Компилятор</a> C$<br>
+      Последние изменения в ветке: 28/12/2025
+    <div>
+    <br><br>
+    <h4>
+      Имя файла: <?=$file_name_3.'.'.$exe?><br>
+      Версия: 0.X (Beta)<br>
+      Дата публикации: XX/XX/202X<br>
+      Размер файла: 0 байт
+    </h4>
+    <h4>
+      <a href=<?=$file_name_3.'.'.$txt?> target="blank"><button>Содержимое файла</button></a>&nbsp;&nbsp;::&nbsp;&nbsp;<a href=<?=$file_name_3.'.'.$exe?> download><button>Скачать</button></a>
+    </h4>
+    <br>
+    <h4>
+      Имя файла: <?=$file_name_3.'.'.$exe?><br>
+      Версия: 0.X (Release)<br>
+      Дата публикации: XX/XX/202X<br>
+      Размер файла: 0 байт
+    </h4>
+    <h4>
+      <a href=<?=$file_name_3.'.'.$txt?> target="blank"><button>Содержимое файла</button></a>&nbsp;&nbsp;::&nbsp;&nbsp;<a href=<?=$file_name_3.'.'.$exe?> download><button>Скачать</button></a>
+    </h4>
+    <br><hr>
+    <copy>© 2026 | Все права защищены</copy>
+  </center>
+</body>
+</html>
+<!---
+<h1>Демонстрация показа кода</h1>
+<form>
+    <label for="language">Выберите язык:</label>
+    <select id="language">
+        <option value="javascript">JavaScript</option>
+        <option value="python">Python</option>
+        <option value="html">HTML</option>
+    </select>
+    <button type="button" id="showCodeBtn">Показать код</button>
+</form
+-->
