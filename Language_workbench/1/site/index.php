b0cea3e860d46bfba1fb3e91f9cb1f1aa8b0c47a
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-12-30 19:19:42 +1100

    Add files via upload

--- Language_workbench/1/site/index.php
+++ Language_workbench/1/site/index.php
@@ -4,6 +4,19 @@
   $file_name_1 = "Interpreter_C\$_0.x_";
   $file_name_2 = "VM_C\$_0.x_";
   $file_name_3 = "Compiler_C\$_0.x_";
+  $nbsp_1 = "&nbsp;";
+  $nbsp_2 = "&nbsp;&nbsp;";
+  $nbsp_3 = "&nbsp;&nbsp;&nbsp;";
+  $nbsp_4 = "&nbsp;&nbsp;&nbsp;&nbsp;";
+  $nbsp_5 = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+  $nbsp_6 = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+  $nbsp_7 = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+  $nbsp_8 = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+  $nbsp_10 = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+  $nbsp_11 = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+  $nbsp_12 = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+  $nbsp_14 = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+  $nbsp_17 = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -20,11 +33,60 @@
 <body style="background-color: #F0F0F0;">
   <center>
     <h3>
-      Дата создания сайта: 28/12/2025 | Последнее обновление: 29/12/2025<br>
+      Дата создания сайта: 28/12/2025 | Последнее обновление: 30/12/2025<br>
       /!\ Убедительная просьба дождаться окончания реконструкции сайта! Окончательный запуск сайта будет произведён в начале января 2026 года.
     </h3>
     <hr>
-    Операционная система: Windows 10+<br>
+  </center>
+
+<br>
+<div style="text-align: center;">
+  <div style="display: inline-block; text-align: left; margin: 0; font-family: Courier New; font-size: 14px;">
+    <b>
+<?=$nbsp_2?>Операционная система<br>
+<?=$nbsp_2?>│<br>
+<?=$nbsp_2?>└─›Windows 10+<br>
+   <?=$nbsp_5?>│<br>
+   <?=$nbsp_5?>└─›Программное обеспечение<br>
+   <?=$nbsp_8?>│<br>
+   <?//<!-- Интерпретатор C$ -->?>
+   <?=$nbsp_8?>├︎─︎›︎<a href="https://ru.wikipedia.org/wiki/%D0%98%D0%BD%D1%82%D0%B5%D1%80%D0%BF%D1%80%D0%B5%D1%82%D0%B0%D1%82%D0%BE%D1%80" target="_blank">Интерпретатор C$</a><br>
+   <?=$nbsp_8?>│<?=$nbsp_2?>│<br>
+   <?=$nbsp_8?>│<?=$nbsp_2?>└︎─︎›︎Версия<br>
+   <?=$nbsp_8?>│<?=$nbsp_5?>│<br>
+   <?=$nbsp_8?>│<?=$nbsp_5?>├─›Beta-release<br>
+   <?=$nbsp_8?>│<?=$nbsp_5?>│<?=$nbsp_2?>└─›0.X (в разработке)<br>
+   <?=$nbsp_8?>│<?=$nbsp_5?>│<br>
+   <?=$nbsp_8?>│<?=$nbsp_5?>└─›Stable-release<br>
+   <?=$nbsp_8?>│<?=$nbsp_8?>└─›0.X (в разработке)<br>
+   <?=$nbsp_8?>│<br>
+   <?//<!-- Виртуальная машина C$ -->?>
+   <?=$nbsp_8?>├︎─︎›︎<a href="https://ru.wikipedia.org/wiki/%D0%92%D0%B8%D1%80%D1%82%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F_%D0%BC%D0%B0%D1%88%D0%B8%D0%BD%D0%B0" target="_blank">Виртуальная машина C$</a><br>
+   <?=$nbsp_8?>│<?=$nbsp_2?>│<br>
+   <?=$nbsp_8?>│<?=$nbsp_2?>└︎─︎›︎Версия<br>
+   <?=$nbsp_8?>│<?=$nbsp_5?>│<br>
+   <?=$nbsp_8?>│<?=$nbsp_5?>├─›Beta-release<br>
+   <?=$nbsp_8?>│<?=$nbsp_5?>│<?=$nbsp_2?>└─›0.X (в разработке)<br>
+   <?=$nbsp_8?>│<?=$nbsp_5?>│<br>
+   <?=$nbsp_8?>│<?=$nbsp_5?>└─›Stable-release<br>
+   <?=$nbsp_8?>│<?=$nbsp_8?>└─›0.X (в разработке)<br>
+   <?=$nbsp_8?>│<br>
+   <?//<!-- Компилятор C$ -->?>
+   <?=$nbsp_8?>└︎─︎›︎<a href="https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D0%BC%D0%BF%D0%B8%D0%BB%D1%8F%D1%82%D0%BE%D1%80" target="_blank">Компилятор C$</a><br>
+     <?=$nbsp_11?>│<br>
+     <?=$nbsp_11?>└︎─︎›︎Версия<br>
+        <?=$nbsp_14?>│<br>
+        <?=$nbsp_14?>├─›Beta-release<br>
+<?=$nbsp_14?>│<?=$nbsp_2?>└─›0.X (в разработке)<br>
+<?=$nbsp_14?>│<br>
+        <?=$nbsp_14?>└─›Stable-release<br>
+           <?=$nbsp_17?>└─›0.X (в разработке)<br>
+    </b>
+  </div>
+</div>
+<br>
+
+    <center>
     <hr><br>
     <div>
       Программное обеспечение: <a href="https://ru.wikipedia.org/wiki/%D0%98%D0%BD%D1%82%D0%B5%D1%80%D0%BF%D1%80%D0%B5%D1%82%D0%B0%D1%82%D0%BE%D1%80" target="blank">Интерпретатор</a> C$<br>
@@ -101,7 +163,7 @@
       <a href=<?=$file_name_3.'.'.$txt?> target="blank"><button>Содержимое файла</button></a>&nbsp;&nbsp;::&nbsp;&nbsp;<a href=<?=$file_name_3.'.'.$exe?> download><button>Скачать</button></a>
     </h4>
     <br><hr>
-    <copy>© 2026 | Все права защищены</copy>
+    <copy>© Meltrune 2026</copy>
   </center>
 </body>
 </html>
