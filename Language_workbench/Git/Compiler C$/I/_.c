bcf90aec75ce1c926c6d2cfae640b4f74176bb7a
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-01-03 06:29:40 +1100

    Add files via upload

--- Language_workbench/Git/Compiler C$/I/_.c
+++ Language_workbench/Git/Compiler C$/I/_.c
@@ -2,21 +2,54 @@
 #include <locale.h>
 #include <string.h>
 
+// таблица имён токенов
+char table_token_name[][40+1] =
+{
+   {"NUMBER"},
+   {"OP_MUL"},
+   {"OP_DIV"},
+   {"OP_ADD"},
+   {"OP_SUB"}
+};
+
+int __number_of_tokens = 0;
+char *token__name[40];  // имя токена
+char *token__value[40]; // значение токена
+
 void lexical_synthesizer(const char *strinp)
 {
    int pos_cursor = 0;
    while (*strinp != '\0')
    {
-      while (*strinp >= '0' && *strinp <= '9')
+      if (*strinp >= '0' && *strinp <= '9')
       {
          pos_cursor++;
          strinp++;
+         if (*strinp == '\0') return;
+         while (*strinp >= '0' && *strinp <= '9')
+         {
+            pos_cursor++;
+            strinp++;
+            if (*strinp == '\0') break;
+         }
+         //printf("\nTOKEN__NUMBER");
+         printf("\n[До]: token: {name: %s, value: %s}", token__name[__number_of_tokens], token__value[__number_of_tokens]);
+         token__name[__number_of_tokens] = "NUMBER";
+         token__value[__number_of_tokens] = (char *) &strinp[__number_of_tokens];
+         token__value[__number_of_tokens][pos_cursor] = '\0';
+         printf("\n[После]: token: {name: %s, value: %s}", token__name[__number_of_tokens], token__value[__number_of_tokens]);
+         __number_of_tokens++;
       }
-      printf("\nTOKEN__NUMBER");
       switch (*strinp)
       {
          case '*':
-            printf("\nTOKEN__OP_MUL");
+            //printf("\nTOKEN__OP_MUL");
+            printf("\n[До]: token: {name: %s, value: %s}", token__name[__number_of_tokens], token__value[__number_of_tokens]);
+            token__name[__number_of_tokens] = "OP_MUL";
+            token__value[__number_of_tokens] = (char *) &strinp[__number_of_tokens];
+            token__value[__number_of_tokens][pos_cursor] = '\0';
+            printf("\n[После]: token: {name: %s, value: %s}", token__name[__number_of_tokens], token__value[__number_of_tokens]);
+            __number_of_tokens++;
             pos_cursor++;
             strinp++;
             break;
