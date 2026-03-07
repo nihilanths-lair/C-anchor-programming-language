0885d9eca9c7fc87ecf099650448d44e6ac59d18
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-03-07 15:59:25 +1100

    Add files via upload

--- src/programs/emulators/written in C/Emu_vCPU.c
+++ src/programs/emulators/written in C/Emu_vCPU.c
@@ -1,4 +1,22 @@
+#include <stdint.h>
+#include <locale.h>
+#include <stdio.h>
+
+uint8_t vIP = 0;
+
+char * ProcAsciiChr(char chr)
+{
+    switch (chr) {
+    case '\0': return "\0";
+    }
+}
+
 int main()
 {
-    
+    setlocale(0, "");
+
+    printf("    HEX   DEC   ASCII\n");
+    for (int i = 0; i < 15; i++) printf("IP  [%02X]\t[%03d]\t[%c]\n", vIP, vIP, vIP), vIP++;
+
+    return 0;
 }
