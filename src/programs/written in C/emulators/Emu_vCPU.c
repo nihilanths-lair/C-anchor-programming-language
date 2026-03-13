359f420d2e60a135c220d61d775c762fc886fa95
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-03-13 12:30:35 +1100

    Add files via upload

--- src/programs/written in C/emulators/Emu_vCPU.c
+++ src/programs/written in C/emulators/Emu_vCPU.c
@@ -8,32 +8,9 @@
 #define _rb_ {
 #define _eb_ }
 
-char ProcAsciiChr(unsigned char chr)
-{
-    switch (chr) {
-    case '\0'/*000*/: return '·';
-    case 0x07/*007*/: return '·';
-    case 0x08/*008*/: return '·';
-    case 0x09/*009*/: return '·';
-    case '\n'/*010*/: return '·';
-    case '\r'/*013*/: return '·';
-    case 0x1b/*027*/: return '·';
-    case 0x95/*149*/: return '·';
-    // 30-39 или 048-057: 0-9
-    // 41-5A или 065-090: A-Z
-    // 61-7A или 097-122: a-z
-    //    A8 или     168: Ё
-    //    B8 или     184: ё
-    // C0-DF или 192-223: А-Я
-    // E0-FF или 224-255: а-я
-    default: return chr;
-    }
-}
-
 uint8_t vIP;
 uint8_t vSP;
-uint8_t vDI;
-uint8_t vSI;
+uint8_t vDI, vSI;
 
 uint8_t vMEMORY[0xFF];
 uint8_t vSTACK[0xFF];
@@ -43,7 +20,69 @@ char * _ptr_data_1 = _data_1;
 
 char processed_text[0xFF];
 unsigned char idx__processed_text;
-//char _data_2[0xFF];
+
+enum UppercaseLetters // Заглавные буквы
+{
+    MOV = 1,
+    INC, DEC,
+    ADD, SUB, MUL, DIV,
+    JMP,
+    PUSH,
+    INT,
+};
+enum LowercaseLetters // Строчные буквы
+{
+    mov = 1,
+    inc, dec,
+    add, sub, mul, div,
+    jmp,
+    push,
+    _int,
+};
+
+// Syntax AT&T / Intel
+unsigned char opcode[] =
+{
+    /*
+    MOV, 0, 'Г', // MOV 0, 'Г' | 01 00 C3 | 001 000 195 | ··Г
+    MOV, 1, 'л', // MOV 1, 'л' | 01 01 EB | 001 001 235 | ··л
+    MOV, 2, 'е', // MOV 2, 'е' | 01 02 E5 | 001 002 229 | ··е
+    MOV, 3, 'б', // MOV 3, 'б' | 01 03 E1 | 001 003 225 | ··б
+    MOV, 4, '.', // MOV 4, '.' | 01 04 2E | 001 004 046 | ··.
+    INC, 4,      // INC 4      | 02 04    | 002 004     | ··
+    DEC, 4,      // DEC 4      | 03 04    | 003 004     | ··
+    MOV, 255, 5, // MOV 255, 5 | 01 FF 05 | 001 255 005 | ·я·
+    */
+    MOV, 254,  2, // MOV 254, 2 | 01 FE 02 | 001 254 002 | ·ю·
+    //MOV, 255, 3,// MOV 255, 3 | 01 FF 03 | 001 255 003 | ·я·
+    ADD, 254,  3, // ADD 254, 3 | 04 FE 03 | 004 254 003 | ·ю·
+
+    MOV, 255, 11, // MOV 255, 11 | 01 FF 0B | 001 255 011 | ·я·
+    SUB, 255,  4, // SUB 255, 4  | 05 FF 04 | 005 255 004 | ·я·
+    EOF
+};
+
+char ProcAsciiChr(unsigned char chr)
+{
+    switch (chr)_rb_
+    case '\0'/*000*/: return '·';
+    case 0x07/*007*/: return '·';
+    case 0x08/*008*/: return '·';
+    case 0x09/*009*/: return '·';
+    case '\n'/*010*/: return '·';
+    case '\r'/*013*/: return '·';
+    case 0x1B/*027*/: return '·';
+    case 0x95/*149*/: return '·';
+    // 30-39 или 048-057: 0-9
+    // 41-5A или 065-090: A-Z
+    // 61-7A или 097-122: a-z
+    //    A8 или     168: Ё
+    //    B8 или     184: ё
+    // C0-DF или 192-223: А-Я
+    // E0-FF или 224-255: а-я
+    default: return chr;
+    _eb_
+}
 
 /// ... ///
 /// метка: оператор операнд-1 операнд-2
@@ -254,15 +293,22 @@ void Preprocessing(char * text, unsigned char preprocessing_type, size_t file_si
     */
 }
 
+void Execute() // Launch
+{
+    printf("\n>> Execute()\n");
+
+    
+}
+
 void Compile(char * text, size_t file_size)
 {
-    printf("\n>> Compile()");
+    printf("\n>> Compile()\n");
     Preprocessing(text, 2, file_size);
 }
 
 void LoadingProgramIntoMemory(const char * opcode)
 {
-    puts("\n>> LoadingProgramIntoMemory()");
+    puts("\n>> LoadingProgramIntoMemory()\n");
     for (int i = 0; opcode[i] != -1; i++) vMEMORY[i] = opcode[i];
     //for ( ; opcode[i] != '\0'; i++) vMEMORY[i] = '\0';
     //printf("opcode >> %lld", len);
@@ -274,11 +320,11 @@ void ShowDashboard()
     printf("\n    HEX | DEC | ASCII\n");
     //for (int i = 0; i < 256; i++)
     printf(" IP: %02X | %03d | %c\n", vIP, vIP, ProcAsciiChr(vIP));
-    printf(" SP: %02X | %03d | %c\n", vSP, vSP, ProcAsciiChr(vSP));
+    //printf(" SP: %02X | %03d | %c\n", vSP, vSP, ProcAsciiChr(vSP));
     //printf("DI  [%02X]  [%03d]  [%c]\n", vDI, vDI, ProcAsciiChr(vDI));
     //printf("SI  [%02X]  [%03d]  [%c]\n", vSI, vSI, ProcAsciiChr(vSI));
-    char op = 0;
-    switch (op){
+    //char op = 0;
+    switch (0)_rb_
     case 0:
     {
         printf("\n MEMORY (HEX | DEC | ASCII)\n    ");
@@ -317,7 +363,8 @@ void ShowDashboard()
             printf("\n[%02X]", j * (0x0F + 1));
             for (int i = 0; i < 0x0F + 1; i++) printf(" %02X ", vMEMORY[i]);
         }
-    }}
+    }_eb_
+    putchar('\n');
 }
 
 // Шаг вперёд
@@ -325,24 +372,6 @@ void StepForward(){}
 // Шаг назад
 //void StepBack(){}
 
-enum UppercaseLetters // Заглавные буквы
-{
-    MOV = 1,
-    INC, DEC,
-    ADD, SUB, MUL, DIV,
-    JMP,
-    PUSH,
-    INT,
-};
-enum LowercaseLetters // Строчные буквы
-{
-    mov = 1,
-    inc, dec,
-    add, sub, mul, div,
-    jmp,
-    push,
-    _int,
-};
 struct TableOpcode {
     unsigned char identifier;
     char symbolic_name[0xF+1];
@@ -361,12 +390,37 @@ int main()
     setlocale(0, "");
 
     char cmd[128+1];
-    while (false)
+    puts("\nДля отображения списка команд введите: /cmdlist");
+    while (true)
     {
-        printf("CMD> ");
+        printf("\n>~@~> ");
 
         fgets(cmd, sizeof (cmd), stdin); // fgets считывает строку включая пробелы и '\n'
         cmd[strcspn(cmd, "\n")] = '\0';  // Удаляем символ переноса строки '\n', если он есть
+
+        if (!strcmp(cmd, "/cmdlist")) printf("1] /compile\n2] /execute");
+        else if (!strcmp(cmd, "/compile"))
+        {
+            unsigned char data[0xFF] = "";
+            unsigned char ptr_data = 0xFF;
+            // Считываем с файла
+            FILE * desc = fopen("_.asm", "rb");
+            if (desc == NULL) { printf("Ошибка открытия файла."); return -1; }
+            fseek(desc, 0, SEEK_END);
+            size_t file_size = ftell(desc);
+            printf("\nРазмер файла: %ld.\n", file_size);
+            size_t copy_file_size = file_size;
+            fseek(desc, 0, SEEK_SET);
+            while (file_size--) data[++ptr_data] = fgetc(desc);
+            fclose(desc);
+            data[++ptr_data] = '\0';
+
+            printf("\n[file: _.asm]\n'''\n%s\n'''\n", data);
+            //file_size = copy_file_size;
+            Compile(data, copy_file_size);
+        }
+        else if (!strcmp(cmd, "/execute")) printf("2] /!\\");
+        else printf("Неизвестная/неопознанная команда...");
     }
 
     //ShowDashboard();
@@ -376,25 +430,7 @@ int main()
     //vSI = 0x00;
     //ShowDashboard();
 
-    unsigned char data[0xFF] = "";
-    unsigned char ptr_data = 0xFF;
-    FILE *desc;
-    // Считываем с файла
-    desc = fopen("_.asm", "rb");
-    if (desc == NULL) { printf("Ошибка открытия файла."); return -1; }
-    fseek(desc, 0, SEEK_END);
-    size_t file_size = ftell(desc);
-    printf("\nРазмер файла: %ld.\n", file_size);
-    size_t copy_file_size = file_size;
-    fseek(desc, 0, SEEK_SET);
-    while (file_size--) data[++ptr_data] = fgetc(desc);
-    fclose(desc);
-    data[++ptr_data] = '\0';
-
-    printf("\n[file: _.asm]\n'''\n%s\n'''\n", data);
-
-    //file_size = copy_file_size;
-    Compile(data, copy_file_size);
+    Execute(opcode); // Launch
     /*
     Compile(
         "JMP _\n"
@@ -419,27 +455,7 @@ int main()
     -;
     MOV 3, 'б'
     */
-    // Syntax AT&T / Intel
-    unsigned char opcode[] =
-    {
-        /*
-        MOV, 0, 'Г', // MOV 0, 'Г' | 01 00 C3 | 001 000 195 | ··Г
-        MOV, 1, 'л', // MOV 1, 'л' | 01 01 EB | 001 001 235 | ··л
-        MOV, 2, 'е', // MOV 2, 'е' | 01 02 E5 | 001 002 229 | ··е
-        MOV, 3, 'б', // MOV 3, 'б' | 01 03 E1 | 001 003 225 | ··б
-        MOV, 4, '.', // MOV 4, '.' | 01 04 2E | 001 004 046 | ··.
-        INC, 4,      // INC 4      | 02 04    | 002 004     | ··
-        DEC, 4,      // DEC 4      | 03 04    | 003 004     | ··
-        MOV, 255, 5, // MOV 255, 5 | 01 FF 05 | 001 255 005 | ·я·
-        */
-        MOV, 254,  2, // MOV 254, 2 | 01 FE 02 | 001 254 002 | ·ю·
-        //MOV, 255, 3,// MOV 255, 3 | 01 FF 03 | 001 255 003 | ·я·
-        ADD, 254,  3, // ADD 254, 3 | 04 FE 03 | 004 254 003 | ·ю·
-
-        MOV, 255, 11, // MOV 255, 11 | 01 FF 0B | 001 255 011 | ·я·
-        SUB, 255,  4, // SUB 255, 4  | 05 FF 04 | 005 255 004 | ·я·
-        EOF
-    };
+    ////////////////////
     LoadingProgramIntoMemory(opcode);
     vIP = 0; // Инициализация
     //printf("vIP = %d", vIP);
