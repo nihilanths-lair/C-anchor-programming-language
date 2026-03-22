d74298851685419d1f81d3fd31686ffe55db44fb
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-03-22 18:17:59 +1100

    Add files via upload

---
+++ src/programs/written in C/VM/v.0.1/vm.c
@@ -0,0 +1,10 @@
+// <!-- Virtual Machine C$ v.0.1 (Encoding: Windows-1251) -->
+
+#include <locale.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    setlocale(0, "");
+    return 0;
+}
