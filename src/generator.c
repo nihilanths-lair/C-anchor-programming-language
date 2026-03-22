7d71fe7d4fdc57c1eff3874dc976bd181c6fe958
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-03-22 18:06:04 +1100

    Add files via upload

---
+++ src/generator.c
@@ -0,0 +1,43 @@
+// <!-- Software generator C$ v.0.1 (Encoding: Windows-1251) -->
+
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
+
+const char format[0xFFFF] =
+{"\
+// <!-- %s v.0.1 (Encoding: Windows-1251) -->\n\
+\n\
+#include <locale.h>\n\
+#include <stdio.h>\n\
+\n\
+int main(int argc, char *argv[])\n\
+{\n\
+    setlocale(0, \"\");\n\
+    return 0;\n\
+}\
+"};
+
+FILE *file;
+//
+int main()
+{
+    setlocale(0, "");
+    //
+    file = fopen("src\\programs\\written in C\\Interpreter\\v.0.1\\interpreter.c", "w");
+    fprintf(file, format, "Interpreter C$");
+    //
+    fclose(file);
+
+    file = fopen("src\\programs\\written in C\\Compiler\\v.0.1\\compiler.c", "w");
+    fprintf(file, format, "Compiler C$");
+    //
+    fclose(file);
+
+    file = fopen("src\\programs\\written in C\\VM\\v.0.1\\vm.c", "w");
+    fprintf(file, format, "Virtual Machine C$");
+    //
+    fclose(file);
+    //
+    return 0;
+}
