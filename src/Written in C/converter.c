90a0de64ea0f9347ff7770928e937d8bfba9da5d
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-04-01 19:02:57 +1100

    Add files via upload

--- src/Written in C/converter.c
+++ src/Written in C/converter.c
@@ -7,21 +7,21 @@ unsigned char input_data[0xFFFFFF] = "\
 unsigned char output_data[0xFFFFFF] = "\
 ";
 
-void hextxt_to_ascii(){}//16
-void dectxt_to_ascii(){}//10
-void bintxt_to_ascii(){}//2
-void ascii_to_hextxt(){}
-void ascii_to_dectxt(){}
-void ascii_to_bintxt(){}
+void _16txt_to_ascii(){}
+void _10txt_to_ascii(){}
+void _2txt_to_ascii(){}
+void ascii_to_16txt(){}
+void ascii_to_10txt(){}
+void ascii_to_2txt(){}
 
 void cnv(const char *input_format, const char *output_format)
 {
-    /***/if (!strcmp(input_format, "hex.txt") && !strcmp(output_format, "ascii.txt")) hextxt_to_ascii();
-    else if (!strcmp(input_format, "dec.txt") && !strcmp(output_format, "ascii.txt")) dectxt_to_ascii();
-    else if (!strcmp(input_format, "bin.txt") && !strcmp(output_format, "ascii.txt")) bintxt_to_ascii();
-    else if (!strcmp(input_format, "ascii") && !strcmp(output_format, "hex.txt")) ascii_to_hextxt();
-    else if (!strcmp(input_format, "ascii") && !strcmp(output_format, "dec.txt")) ascii_to_dectxt();
-    else if (!strcmp(input_format, "ascii") && !strcmp(output_format, "bin.txt")) ascii_to_bintxt();
+    /***/if (!strcmp(input_format, "hex.txt") && !strcmp(output_format, "ascii")) _16txt_to_ascii();
+    else if (!strcmp(input_format, "dec.txt") && !strcmp(output_format, "ascii")) _10txt_to_ascii();
+    else if (!strcmp(input_format, "bin.txt") && !strcmp(output_format, "ascii")) _2txt_to_ascii();
+    else if (!strcmp(input_format, "ascii") && !strcmp(output_format, "hex.txt")) ascii_to_16txt();
+    else if (!strcmp(input_format, "ascii") && !strcmp(output_format, "dec.txt")) ascii_to_10txt();
+    else if (!strcmp(input_format, "ascii") && !strcmp(output_format, "bin.txt")) ascii_to_2txt();
 }
 
 int main()
