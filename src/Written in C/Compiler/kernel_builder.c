435f382850a8561453230fb925375b8c06084ce8
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-04-05 17:37:23 +1100

    Add files via upload

---
+++ src/Written in C/Compiler/kernel_builder.c
@@ -0,0 +1,11 @@
+// <!-- Kernel builder v.0.1 (Encoding: Windows-1251) -->
+
+#include <locale.h>
+#include <stdio.h>
+
+void _()
+{
+    setlocale(0, "");
+    // ... //
+}
+int main(int argc, char *argv[]) { _(argc, argv); }
