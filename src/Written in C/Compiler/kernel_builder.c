77ec161f33a2acebab359d19c11195c6ad863640
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-04-05 19:09:53 +1100

    Add files via upload

--- src/Written in C/Compiler/kernel_builder.c
+++ src/Written in C/Compiler/kernel_builder.c
@@ -11,18 +11,39 @@ void _(int argc, char *argv[])
     setlocale(0, "");
     //
     char buf[0xFF];
+    char *ptr_buf = buf;
+
     FILE *file = fopen("_.asm", "rb");
     if (file == NULL) { printf("\n #1\n"); return; }
     size_t read_bytes = fread(buf, 1, sizeof (buf), file);
     buf[read_bytes] = '\0';
     printf("\n```\n%s\n```", buf);
 
-    _1_run: switch (*buf)
+    _1_run: switch (*ptr_buf)
     switch_open
-    case 'J':
-     printf("\n -> 'J'\n");
+
+    case '\0':
+     printf("\n -> '\\0'");
      goto _1_end;
+
+    case 'J':
+     printf("\n -> 'J'");
+     ptr_buf++;
+     goto _1_run;
+
+    case 'M':
+     printf("\n -> 'M'");
+     ptr_buf++;
+     goto _1_run;
+
+    case 'P':
+     printf("\n -> 'P'");
+     ptr_buf++;
+     goto _1_run;
+
     switch_close
     _1_end:
+
+    putchar('\n');
 }
 int main(int argc, char *argv[]) { _(argc, argv); }
