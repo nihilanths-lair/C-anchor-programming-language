a2bc46f94b767d5aa1634ac559bf4615e86dcb20
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-04-05 18:57:52 +1100

    Add files via upload

--- src/Written in C/Compiler/kernel_builder.c
+++ src/Written in C/Compiler/kernel_builder.c
@@ -3,9 +3,26 @@
 #include <locale.h>
 #include <stdio.h>
 
+#define switch_open {
+#define switch_close }
+
 void _(int argc, char *argv[])
 {
     setlocale(0, "");
-    // ... //
+    //
+    char buf[0xFF];
+    FILE *file = fopen("_.asm", "rb");
+    if (file == NULL) { printf("\n #1\n"); return; }
+    size_t read_bytes = fread(buf, 1, sizeof (buf), file);
+    buf[read_bytes] = '\0';
+    printf("\n```\n%s\n```", buf);
+
+    _1_run: switch (*buf)
+    switch_open
+    case 'J':
+     printf("\n -> 'J'\n");
+     goto _1_end;
+    switch_close
+    _1_end:
 }
 int main(int argc, char *argv[]) { _(argc, argv); }
