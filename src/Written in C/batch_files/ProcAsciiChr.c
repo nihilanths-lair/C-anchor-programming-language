7c12b54c6a94c14792d5fe4618a857063254bf29
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-04-17 16:29:25 +1100

    Add files via upload

---
+++ src/Written in C/batch_files/ProcAsciiChr.c
@@ -0,0 +1,51 @@
+const char ProcAsciiChr(unsigned char chr)
+{
+    switch (chr){
+    case '\0': return '·'; // ··0
+    case 0x01: return '·'; // ··1
+    case 0x02: return '·'; // ··2
+    case 0x03: return '·'; // ··3
+    case 0x04: return '·'; // ··4
+    ///case 0x05: return '·'; // ··5
+    case 0x06: return '·'; // ··6
+    case 0x07: return '·'; // ··7
+    case 0x08: return '·'; // ··8
+    case 0x09: return '·'; // ··9
+    case '\n': return '·'; // ·10
+    case 0x0B: return '·'; // ·11
+    case 0x0C: return '·'; // ·12
+    case '\r': return '·'; // ·13
+    //case 0x0E: return '·'; // ·14
+    case 0x0F: return '·'; // ·15
+    case 0x10: return '·'; // ·16
+    case 0x11: return '·'; // ·17
+    case 0x12: return '·'; // ·18
+    case 0x13: return '·'; // ·19
+    case 0x14: return '·'; // ·20
+    case 0x1B: return '·'; // ·27
+    // 30-39 или 048-057: 0-9
+    // 41-5A или 065-090: A-Z
+    // 61-7A или 097-122: a-z
+    case 0x90: return '·'; // 144
+    case 0x95: return '·'; // 149
+    //    A8 или     168: Ё
+    //    B8 или     184: ё
+    // C0-DF или 192-223: А-Я
+    // E0-FF или 224-255: а-я
+    default: return chr;
+    }
+}
+// --
+const char GetDebuggingInformation(const unsigned char *memory, const unsigned char ip8)
+{
+    switch (memory[ip8]){
+    case 0: // 1
+    case 1:
+    case 2:
+    case 3: break;
+    case 4: printf("\n [DEBUG] dispatch__mode_8__identifier_opcode_%d: MEMORY [%d] = %d ; Intel", memory[ip8]+1, memory[ip8+1], memory[memory[ip8+1]]); break;
+    case 5: printf("\n [DEBUG] dispatch__mode_8__identifier_opcode_%d: MEMORY [%d] = %d ; Intel", memory[ip8]+1, memory[ip8+1], memory[memory[ip8+1]]); break;
+    case 6: printf("\n [DEBUG] dispatch__mode_8__identifier_opcode_%d: MEMORY [%d] = %d ; AT&T\n" , memory[ip8]+1, memory[ip8+2], memory[memory[ip8+2]]); break;
+    case 255: printf("\n [DEBUG] dispatch__mode_8__identifier_opcode_%d", memory[ip8]+1);
+    }
+}
