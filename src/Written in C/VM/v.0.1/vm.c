900315ee84527ece800c47d97acef6c12296c054
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-04-19 10:15:12 +1100

    Add files via upload

--- src/Written in C/VM/v.0.1/vm.c
+++ src/Written in C/VM/v.0.1/vm.c
@@ -24,7 +24,7 @@ char meta_description[] = "\
 /*static inline*/void Permutation(void *opcode_identifier_table[], unsigned char table_size,/*const char *opcode_table,*/const char *meta_description)
 {
     //printf("\n <DEBUG>: CALL Permutation\n"); // Эталонный
-    char opcode_table[0xFF];
+    //unsigned char opcode_table[0xFF+1];
     // Парсинг `meta_description` в DOM-структуру: таблица соотношений [идентификатор опкода :: значение опкода]
     unsigned char opcode_configuration_table[0xFF+1][2]; // Первый index идентификатор опкода, второй - значение опкода
     //unsigned char opcode_configuration_table[2][0xFF+1]; // Первый index идентификатор опкода, второй - значение опкода
@@ -32,20 +32,23 @@ char meta_description[] = "\
     #define HLT 0x01 // <%1>
     #define JMP 0x02 // <%2>
     #define INC 0x03 // <%3>
+    unsigned char opcode_table[] = {INC, JMP, HLT};
     opcode_configuration_table[0][value] = INC; // <%3>
     opcode_configuration_table[1][value] = JMP; // <%2>
     opcode_configuration_table[2][value] = HLT; // <%1>
+
+    void *opcode_identifier_table_address[0xFF+1];
     // Временно пропустим данный этап и предположим у нас уже есть готовая таблица, воспользуемся ей
     //printf("\n\n [До пермутации]:\n"); / гасим
-    printf("\n                          Address, Identifier = Opcode: ASCII | HEX | DEC");
-    for (int i = 0; i < table_size; i++)
+    printf("\n\n             Address, Identifier = Opcode: ASCII | HEX | DEC");
+    for (int i = 0; i < sizeof (opcode_table); i++)
     {
-        printf("\n\n    [До пермутации]: 0x%p, <%%%d> = %d | %02Xh | %03dd", opcode_identifier_table[i], i+1, opcode_table[i], opcode_table[i], opcode_table[i]);
+        printf("\n\n    [До пермутации]: %ph, <%%%d> = %d | %02Xh | %03dd", opcode_identifier_table[i], i+1, opcode_table[i], opcode_table[i], opcode_table[i]);
 
-        //printf("\n opcode_table[%d] = %d", i, opcode_table[i]); // Все опкоды (коды операций)
-        //opcode_identifier_table[opcode_table[i]] = opcode_identifier_table[i]; // Каждый опкод (код операции) маппим на внутреннюю логику
+        printf("\n opcode_table[%d] = %d", i, opcode_table[i]); // Все опкоды (коды операций)
+        opcode_identifier_table[opcode_table[i]] = opcode_identifier_table[i]; // Каждый опкод (код операции) маппим на внутреннюю логику
 
-        printf("\n [После пермутации]: 0x%p, <%%%d> = %d | %02Xh | %03dd", opcode_identifier_table[i], i+1, opcode_table[i], opcode_table[i], opcode_table[i]);
+        printf("\n [После пермутации]: %ph, <%%%d> = %d | %02Xh | %03dd", opcode_identifier_table[i], i+1, opcode_table[i], opcode_table[i], opcode_table[i]);
     }
     putchar('\n');
     //printf("\n\n [После пермутации]:"); / гасим
@@ -76,10 +79,11 @@ void _()
     opcode_identifier_4: goto _0;
     opcode_identifier_5: goto _0;
     _0:
-    putchar('\n');
     // Пермутация (внедряется в мета-компиляторы и мета-программируемые виртуальные машины, для сборки и привязки динамически модифицируемых/меняющихся опкодов к единой таблице идентификаторов)
     Permutation(opcode_identifier_table, MACRO__TABLE_SIZE, /*opcode_table,*/"");
     printf("\n [После]: Эталонная таблица идентификаторов опкодов (кодов операций)\n");
+    for (unsigned char i = 0; i < MACRO__TABLE_SIZE; i++) printf("\n %ph: <%%%d> = %d | \\h%02X | \\d%03d", opcode_identifier_table[i], i+1, i, i, i);
+    putchar('\n');
 
     //Action();
     //#include "..\..\batch_files\action.txt"
