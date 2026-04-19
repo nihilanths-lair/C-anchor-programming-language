c54bb0bf16c9561bce6ad1e7f6d595696aefb977
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-04-20 01:54:20 +1100

    Add files via upload

--- src/Written in C/VM/v.0.1/vm.c
+++ src/Written in C/VM/v.0.1/vm.c
@@ -6,9 +6,11 @@
 
 #include "..\..\batch_files\ProcAsciiChr.c"
 #include "..\..\batch_files\ShowDashboard.c"
-#include "..\..\batch_files\Permutation.c"
 
-#include "..\..\batch_files\action.c"
+#include "..\..\batch_files\Permutation.c"
+#include "..\..\batch_files\CheckTableStatus.c"
+
+#include "..\..\batch_files\Action.c"
 
 // Мета-описание для привязки опкодов к своим уникальным адресам/ключам/индексам)
 // <%?> - является идентификатором опкода (кода операции)
@@ -21,48 +23,11 @@ char meta_description[] =
     " <%5> = 5\n"
 ;
 
-void _()
+void _(int argc, char *argv[])
 {
     setlocale(0, "");
     //
-    #define MACRO__TABLE_SIZE 5
-    unsigned char opcode_table[MACRO__TABLE_SIZE] = {
-        0, 1, 2, 3, 4
-    }; // Таблица опкодов (или кодов операций)
-    void *opcode_identifier_table[MACRO__TABLE_SIZE] = {
-        &&opcode_identifier_1,
-        &&opcode_identifier_2,
-        &&opcode_identifier_3,
-        &&opcode_identifier_4,
-        &&opcode_identifier_5
-    }; // Таблица идентификаторов опкодов (или кодов операций)
-    CheckTableStatus(opcode_identifier_table, opcode_table, sizeof (opcode_table));
-    Permutation(opcode_identifier_table, opcode_table, sizeof (opcode_table), meta_description);
-    CheckTableStatus(opcode_identifier_table, opcode_table, sizeof (opcode_table));
-    goto _0;
-
-    opcode_identifier_1:
-     printf("\n opcode_identifier_1 = %ph: <%%%d> = %3d | \\h%02X | \\d%03d", opcode_identifier_table[0], 1, opcode_table[0], opcode_table[0], opcode_table[0]);
-    goto _0;
-
-    opcode_identifier_2:
-     printf("\n opcode_identifier_2 = %ph: <%%%d> = %3d | \\h%02X | \\d%03d", opcode_identifier_table[1], 2, opcode_table[1], opcode_table[1], opcode_table[1]);
-    goto _0;
-
-    opcode_identifier_3:
-     printf("\n opcode_identifier_3 = %ph: <%%%d> = %3d | \\h%02X | \\d%03d", opcode_identifier_table[2], 3, opcode_table[2], opcode_table[2], opcode_table[2]);
-    goto _0;
-
-    opcode_identifier_4:
-     printf("\n opcode_identifier_4 = %ph: <%%%d> = %3d | \\h%02X | \\d%03d", opcode_identifier_table[3], 4, opcode_table[3], opcode_table[3], opcode_table[3]);
-    goto _0;
-
-    opcode_identifier_5:
-     printf("\n opcode_identifier_5 = %ph: <%%%d> = %3d | \\h%02X | \\d%03d", opcode_identifier_table[4], 5, opcode_table[4], opcode_table[4], opcode_table[4]);
-    goto _0;
-    
-    _0:
-    //Action();
+    Action();
     //#include "..\..\batch_files\action.txt"
 }
 int main(int argc, char *argv[]) { _(argc, argv); }
