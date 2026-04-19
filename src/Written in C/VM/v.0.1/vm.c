30b6a0144c2dfdbbb8619fb65cb9e4114ad807c4
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-04-19 11:44:07 +1100

    Add files via upload

--- src/Written in C/VM/v.0.1/vm.c
+++ src/Written in C/VM/v.0.1/vm.c
@@ -24,11 +24,8 @@ char meta_description[] =
 /*static inline*/void Permutation(void *opcode_identifier_table[], unsigned char *opcode_table, const unsigned char table_size, const char *meta_description)
 {
     //printf("\n <DEBUG>: CALL Permutation\n"); // Эталонный
+    //
     printf("\n\n meta_description: \n%s", meta_description);
-    //unsigned char opcode_table[0xFF+1];
-    // Парсинг `meta_description` в DOM-структуру: таблица соотношений [идентификатор опкода :: значение опкода]
-    unsigned char opcode_configuration_table[0xFF+1][2]; // Первый index идентификатор опкода, второй - значение опкода
-    //unsigned char opcode_configuration_table[2][0xFF+1]; // Первый index идентификатор опкода, второй - значение опкода
 
     #define HLT 0x01
     #define JMP 0x02
@@ -38,7 +35,11 @@ char meta_description[] =
     opcode_table[1] = JMP;
     opcode_table[2] = HLT;
 
-    enum { identifier, value };
+    // Парсинг `meta_description` в DOM-структуру: таблица соотношений [идентификатор опкода :: значение опкода]
+    #define MACRO__TABLE 2
+    unsigned char opcode_configuration_table[0xFF+1][MACRO__TABLE]; // Первый index идентификатор опкода, второй - значение опкода
+
+    enum {identifier, value};
 
     opcode_configuration_table[0][identifier] = 1;
     opcode_configuration_table[0][value] = INC; // <%1> = 3
@@ -53,12 +54,12 @@ char meta_description[] =
     for (unsigned char i = 0; i < table_size; i++)
     {
         // делаем резервное копирование перед внесением изменений (на всякий случай, если потребуется хранить первичное состояние)
-        printf("\n\n opcode_identifier_table        [%d] = %ph", i, opcode_identifier_table[i]);
+        printf("\n opcode_identifier_table        [%d] = %ph", i, opcode_identifier_table[i]);
         opcode_identifier_table_address[i] = opcode_identifier_table[i];
-        printf("\n opcode_identifier_table_address[%d] = %ph", i, opcode_identifier_table_address[i]);
+        printf("\n opcode_identifier_table_address[%d] = %ph\n", i, opcode_identifier_table_address[i]);
     }
     // Временно пропустим данный этап и предположим у нас уже есть готовая таблица, воспользуемся ей
-    printf("\n\n Address, Identifier = Opcode: ASCII | HEX | DEC");
+    printf("\n Address, Identifier = Opcode: ASCII | HEX | DEC");
     printf("\n\n [До пермутации]:");
     for (int i = 0; i < sizeof (opcode_table); i++) printf("\n %ph, <%%%d> = %d | %02Xh | %03dd", opcode_identifier_table[i], i+1, i, i, i);
     putchar('\n');
