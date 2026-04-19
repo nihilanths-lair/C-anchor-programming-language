416ef50dfe1fff7329742d497be7efdf854a39d2
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-04-20 01:20:18 +1100

    Add files via upload

---
+++ src/Written in C/batch_files/CheckTableStatus.c
@@ -0,0 +1,11 @@
+// Проверить статус таблицы
+void CheckTableStatus(void *opcode_identifier_table[], unsigned char *opcode_table, const unsigned char table_size)
+{
+    FILE *file = fopen("output.txt", "wb");
+    for (unsigned char i = 0; i < table_size; i++) {
+        //printf("\n %ph: <%%%d> = %d | \\h%02X | \\d%03d", opcode_identifier_table[i], i+1, opcode_table[i], opcode_table[i], opcode_table[i]);
+        fprintf(file, "%ph: <%%%d> = %d | \\h%02X | \\d%03d\n", opcode_identifier_table[i], i+1, opcode_table[i], opcode_table[i], opcode_table[i]);
+    }
+    fprintf(file, "%ph: <%%%d> = %d | \\h%02X | \\d%03d", opcode_identifier_table[table_size], table_size+1, opcode_table[table_size], opcode_table[table_size], opcode_table[table_size]);
+    fclose(file);
+}
