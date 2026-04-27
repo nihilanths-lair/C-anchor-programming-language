e5519f3b2715f7a94e6773ae7c59c27017748264
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-04-28 01:46:53 +1100

    Add files via upload

--- src/Written in C/batch_files/ProcAsciiChr.c
+++ src/Written in C/batch_files/ProcAsciiChr.c
@@ -342,172 +342,7 @@ const unsigned char * ProcAsciiChrEx(const unsigned char chr)
 const char * ProcAsciiChrDecodedText(const char chr)
 {
     /*static*/char * res = "\0\0\0\0";
-    switch (chr){
-    //
-    case 0: res = "\\00 "; break; // Спец. символы
-    case 1: res = "\\01 "; break; //
-    case 2: res = "\\02 "; break; //
-    case 3: res = "\\03 "; break; //
-    case 4: res = "\\04 "; break; //
-    case 5: res = "\\05 "; break; //
-    case 6: res = "\\06 "; break; //
-    case 7: res = "\\07 "; break; //
-    case 8: res = "\\08 "; break; //
-    case 9: res = "\\09 "; break; //
-    case 0x0A: res = "\\0A "; break; //
-    case 0x0B: res = "\\0B "; break; //
-    case 0x0C: res = "\\0C "; break; //
-    case 0x0D: res = "\\0D "; break; //
-    case 0x0E: res = "\\0E "; break; //
-    case 0x0F: res = "\\0F "; break; //
-    case 16: res = "\\10"; break; //
-    case 17: res = "\\11"; break; //
-    case 18: res = "\\12"; break; //
-    case 19: res = "\\13"; break; //
-    case 20: res = "\\14"; break; //
-    case 21: res = "\\15"; break; //
-    case 22: res = "\\16"; break; //
-    case 23: res = "\\17"; break; //
-    case 24: res = "\\18"; break; //
-    case 25: res = "\\19"; break; //
-    case 0x1A: res = "\\1A"; break; //
-    case 0x1B: res = "\\1B"; break; //
-    case 0x1C: res = "\\1C"; break; //
-    case 0x1D: res = "\\1D"; break; //
-    case 0x1E: res = "\\1E"; break; //
-    case 0x1F: res = "\\1F"; break; //
-    case ' ': res = "    "; break; // SP
-    //
-    case 'A': res = " A "; break;
-    case 'B': res = " B "; break;
-    case 'C': res = " C  "; break;
-    case 'D': res = " D "; break;
-    case 'E': res = " E "; break;
-    case 'F': res = " F "; break;
-    case 'G': res = " G "; break;
-    case 'H': res = " H "; break;
-    case 'I': res = " I "; break;
-    case 'J': res = " J "; break;
-    case 'K': res = " K "; break;
-    case 'L': res = " L "; break;
-    case 'M': res = " M "; break;
-    case 'N': res = " N "; break;
-    case 'O': res = " O "; break;
-    case 'P': res = " P "; break;
-    case 'Q': res = " Q "; break;
-    case 'R': res = " R "; break;
-    case 'S': res = " S "; break;
-    case 'T': res = " T "; break;
-    case 'U': res = " U "; break;
-    case 'V': res = " V "; break;
-    case 'W': res = " W "; break;
-    case 'X': res = " X "; break;
-    case 'Y': res = " Y "; break;
-    case 'Z': res = " Z "; break;
-    //
-    case 'a': res = " a "; break;
-    case 'b': res = " b "; break;
-    case 'c': res = " c "; break;
-    case 'd': res = " d "; break;
-    case 'e': res = " e "; break;
-    case 'f': res = " f "; break;
-    case 'g': res = " g "; break;
-    case 'h': res = " h "; break;
-    case 'i': res = " i  "; break;
-    case 'j': res = " j "; break;
-    case 'k': res = " k "; break;
-    case 'l': res = " l "; break;
-    case 'm': res = " m "; break;
-    case 'n': res = " n "; break;
-    case 'o': res = " o "; break;
-    case 'p': res = " p "; break;
-    case 'q': res = " q "; break;
-    case 'r': res = " r "; break;
-    case 's': res = " s "; break;
-    case 't': res = " t "; break;
-    case 'u': res = " u "; break;
-    case 'v': res = " v "; break;
-    case 'w': res = " w "; break;
-    case 'x': res = " x "; break;
-    case 'y': res = " y "; break;
-    case 'z': res = " z "; break;
-    //
-    case '.': res = " . "; break;
-    case '!': res = " ! "; break;
-    case '#': res = " # "; break; // 23h - 035d
-    case '$': res = " $ "; break; // 24h - 036d
-    case '(': res = " ( "; break; // 28h - 040d
-    //
-    case 'А': res = " А "; break;
-    case 'Б': res = " Б "; break;
-    case 'В': res = " В "; break;
-    case 'Г': res = " Г "; break;
-    case 'Д': res = " Д "; break;
-    case 'Е': res = " Е "; break;
-    case 'Ё': res = " Ё "; break;
-    case 'Ж': res = " Ж "; break;
-    case 'З': res = " З "; break;
-    case 'И': res = " И "; break;
-    case 'Й': res = " Й "; break;
-    case 'К': res = " К "; break;
-    case 'Л': res = " Л "; break;
-    case 'М': res = " М "; break;
-    case 'Н': res = " Н "; break;
-    case 'О': res = " О "; break;
-    case 'П': res = " П "; break;
-    case 'Р': res = " Р "; break;
-    case 'С': res = " С "; break;
-    case 'Т': res = " Т "; break;
-    case 'У': res = " У "; break;
-    case 'Ф': res = " Ф "; break;
-    case 'Х': res = " Х "; break;
-    case 'Ц': res = " Ц "; break;
-    case 'Ч': res = " Ч "; break;
-    case 'Ш': res = " Ш "; break;
-    case 'Щ': res = " Щ "; break;
-    // Слов, начинающихся с заглавного твёрдого знака не существует
-    case 'Ы': res = " Ы "; break; // Спорно..
-    // Слов, начинающихся с заглавного мягкого знака не существует
-    case 'Э': res = " Э "; break;
-    case 'Ю': res = " Ю "; break;
-    case 'Я': res = " Я "; break;
-    //
-    case 'а': res = " а "; break;
-    case 'б': res = " б "; break;
-    case 'в': res = " в "; break;
-    case 'г': res = " г "; break;
-    case 'д': res = " д "; break;
-    case 'е': res = " е "; break;
-    case 'ё': res = " ё "; break;
-    case 'ж': res = " ж "; break;
-    case 'з': res = " з "; break;
-    case 'и': res = " и "; break;
-    case 'й': res = " й "; break;
-    case 'к': res = " к "; break;
-    case 'л': res = " л "; break;
-    case 'м': res = " м "; break;
-    case 'н': res = " н "; break;
-    case 'о': res = " о "; break;
-    case 'п': res = " п "; break;
-    case 'р': res = " р "; break;
-    case 'с': res = " с "; break;
-    case 'т': res = " т "; break;
-    case 'у': res = " у "; break;
-    case 'ф': res = " ф "; break;
-    case 'х': res = " х "; break;
-    case 'ц': res = " ц "; break;
-    case 'ч': res = " ч "; break;
-    case 'ш': res = " ш "; break;
-    case 'щ': res = " щ "; break;
-    case 'ъ': res = " ъ "; break;
-    case 'ы': res = " ы "; break;
-    case 'ь': res = " ь "; break;
-    case 'э': res = " э "; break;
-    case 'ю': res = " ю "; break;
-    case 'я': res = " я "; break;
-    //
-    default: printf("\n ProcAsciiChrDecodedText = %c", chr); break;
-    }
+    printf("\n ProcAsciiChrDecodedText = %c", chr);// break;
     return res;
 }
 
@@ -562,7 +397,7 @@ const char * valstr(unsigned char val)
     }
     return ptr__val_str;
 }
-void mem_dbg(const char *m)
+void mem_dbg(const unsigned char *m)
 {
     //static buf[0xFF];
     printf("\n <DEC>");
@@ -570,7 +405,7 @@ void mem_dbg(const char *m)
     printf("\t <HEX>");
     for (int i = 0; i < 8; i++) printf(" %02X", i);
     printf("\t <ASCII> "); // графа ASCII
-    for (int i = 0; i < 8; i++) printf("%s", ProcAsciiChrEx((unsigned char) i));
+    for (int i = 0; i < 8; i++) printf("%s", ProcAsciiChrEx(i));
     //putchar('\n');
     //printf("\n ------------------------------------------------------------------------------------------------------------------------\n");
     // Данные в памяти
@@ -579,24 +414,24 @@ void mem_dbg(const char *m)
         j2 = j*16;
         printf("\n\n");
         if (j2 >= 0 && j2 <= 255) printf("  %03d:", j2); // графа в данных DEC (1-я часть)
-        for (int i = 0; i < 8; i++) printf(" %03d", (unsigned char) m[j2+i]); // сами данные
+        for (int i = 0; i < 8; i++) printf(" %03d", m[j2+i]); // сами данные
         putchar('\t');
         if (j2 >= 0 && j2 <= 255) printf("   %02X:", j2); // графа в данных HEX (1-я часть)
-        for (int i = 0; i < 8; i++) printf(" %02X", (unsigned char) m[j2+i]); // сами данные
+        for (int i = 0; i < 8; i++) printf(" %02X", m[j2+i]); // сами данные
         putchar('\t');
-        if (j2 >= 0 && j2 <= 255) printf("  %s: ", ProcAsciiChrEx((unsigned char) j2)); // графа в данных ASCII (1-я часть)
-        for (int i = 0; i < 8; i++) printf("%s", ProcAsciiChrEx((unsigned char) m[j2+i])); // сами данные
+        if (j2 >= 0 && j2 <= 255) printf("  %s: ", ProcAsciiChrEx(j2)); // графа в данных ASCII (1-я часть)
+        for (int i = 0; i < 8; i++) printf("%s", ProcAsciiChrEx(m[j2+i])); // сами данные
         putchar('\n');
         //printf("\n\n");
         //printf("\n ------------------------------------------------------------------------------------------------------------------------\n");
         if (j2 >= 0 && j2 <= 255) printf("  %03d:", j2+8); // графа в данных DEC (2-я часть)
-        for (int i = 8; i < 16; i++) printf(" %03d", (unsigned char) m[j2+i]); // сами данные
+        for (int i = 8; i < 16; i++) printf(" %03d", m[j2+i]); // сами данные
         putchar('\t');
         if (j2 >= 0 && j2 <= 255) printf("   %02X:", j2+8); // графа в данных HEX (2-я часть)
-        for (int i = 8; i < 16; i++) printf(" %02X", (unsigned char) m[j2+i]); // сами данные
+        for (int i = 8; i < 16; i++) printf(" %02X", m[j2+i]); // сами данные
         putchar('\t');
-        if (j2 >= 0 && j2 <= 255) printf("  %s: ", ProcAsciiChrEx((unsigned char) j2+8)); // графа в данных ASCII (2-я часть)
-        for (int i = 8; i < 16; i++) printf("%s", ProcAsciiChrEx((unsigned char) m[j2+i])); // сами данные
+        if (j2 >= 0 && j2 <= 255) printf("  %s: ", ProcAsciiChrEx(j2+8)); // графа в данных ASCII (2-я часть)
+        for (int i = 8; i < 16; i++) printf("%s", ProcAsciiChrEx(m[j2+i])); // сами данные
         //putchar('\n');
         //printf("\n\n");
         //printf("\n ------------------------------------------------------------------------------------------------------------------------\n");
