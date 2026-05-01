5eb2e1b6dc413c4e7fcc8594c751d342fc7df4d1
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-01 21:09:27 +1100

    Add files via upload

--- src/Written in C/temporary_compiler.c
+++ src/Written in C/temporary_compiler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <locale.h>
 //
 enum
 {
@@ -24,7 +25,9 @@ struct Parser { char * cursor; } parser; // global object's
 /*/
 void _$()
 {
-    char * code = "5"; // inline-код для быстрого тестирования (временно)
+    setlocale(0, "");
+    //
+    char * code = "x = 5,;"; // inline-код для быстрого тестирования (временно)
     lexer.cursor = code;
     char state_vector = 0; // state vector / вектор состояний
     // transaction_codes / коды транзакций
@@ -32,25 +35,30 @@ void _$()
     {
         switch (*code){
         case '\0': goto while_end;
-        case ' ': ++code; continue; // игнорируем пробельные символы (в данной реализации опустим их)
+        case ' ': { printf("\n Символ: '\\d%03d', '%c', '\\h%02X'.", *code, *code, *code); ++code; } break; // игнорируем пробельные символы (в данной реализации опустим их)
         break; // заглушка (на всякий случай)
 
         case '+': {} break; // '\d043'
         case '-': {} break; // '\d045'
 
+        case ';': { printf("\n Символ: '\\d%03d', '%c', '\\h%02X'.", *code, *code, *code); ++code; } break; // '\d044', ',', '\h2C'
+
         case '<': {} break; // '\d060' / сдвинуть указатель на шаг назад
+        case '=': { printf("\n Символ: '\\d%03d', '%c', '\\h%02X'.", *code, *code, *code); ++code; } break; // '\d061'
         case '>': {} break; // '\d062' / сдвинуть указатель на шаг вперёд
 
-        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': ++code; break;
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+         { printf("\n Символ: '\\d%03d', '%c', '\\h%02X'.", *code, *code, *code); ++code; } break;
 
         case '_':
         case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': // 13
         case 'N': case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z': // 26
 
         case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': // 13
-        case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z': ++code; break; // 26
+        case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+         { printf("\n Символ: '\\d%03d', '%c', '\\h%02X'.", *code, *code, *code); ++code; } break; // '\d097'~'\d122', 'a'~'z', '\h78'~'\h7A' / 26
 
-        default: printf("\n Неизвестный символ: '\\x%X'.", *code);
+        default: printf("\n Неизвестный символ: '\\d%03d', '%c', '\\h%02X'.", *code, *code, *code); ++code;
         }
     }
     while_end:
