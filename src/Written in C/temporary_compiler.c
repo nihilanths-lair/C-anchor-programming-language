53cc9e6e77f6c1fcd7d9aaa27d956992ba703be0
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-01 13:39:14 +1100

    Add files via upload

--- src/Written in C/temporary_compiler.c
+++ src/Written in C/temporary_compiler.c
@@ -33,7 +33,17 @@ void _$()
     {
         switch (*code){
         case '\0': goto while_end;
-        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': ++code;
+        case ' ': ++code; continue; // игнорируем пробельные символы (в данной реализации опустим их)
+        break; // заглушка (на всякий случай)
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': ++code; break;
+        case '_':
+        case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': // 13
+        case 'N': case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z': // 26
+
+        case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': // 13
+        case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z': ++code; break; // 26
+
+        default: printf("\n Неизвестный символ: '\\x%X'.", *code);
         }
     }
     while_end:
