48f6d357d6a12a389ee174cc0436449b3e087bed
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-16 11:54:52 +1100

    Add files via upload

--- src/Written in C/vm.c
+++ src/Written in C/vm.c
@@ -1,8 +1,8 @@
 //#define MACRO__VIRTUAL_ADDRESS (cs8 << 8) + ip8 // максимально допустимая при двух 8-ми битных регистрах
 //#define MACRO__VIRTUAL_ADDRESS (cs16 << 8) + ip16 // максимально допустимая при двух 16-ти битных регистрах
 
-unsigned char gl__memory_tape[MACRO__MAXIMUM_CODE_LIMIT]; // плоская модель памяти, стек находится здесь же (заполнение с конца)
-unsigned char * gl__ptr__memory_tape = gl__memory_tape;
+unsigned char ga__memory_tape[MACRO__MAXIMUM_CODE_LIMIT]; // плоская модель памяти, стек находится здесь же (заполнение с конца)
+unsigned char * gp__memory_tape = ga__memory_tape;
 
 uint8_t cs8 = 0; // (unsigned char) 8-bit's сегментный-регистр
 uint8_t ip8 = 0; // (unsigned char) 8-bit's регистр-указатель на инструкцию
@@ -46,94 +46,93 @@ void Loader_VM()
 {
     //printf("\n    До | %03d %03d %03d = %02X %02X %02X", opcodes[0], opcodes[1], opcodes[2], opcodes[0], opcodes[1], opcodes[2]);
     //printf("\n    До | %03d %03d %03d = %02X %02X %02X", memory_tape[0], memory_tape[1], memory_tape[2], memory_tape[0], memory_tape[1], memory_tape[2]);
-    int len = strlen(gl__opcodes);//+1
-    for (int i = 0; i < len; i++) gl__memory_tape[i] = gl__opcodes[i];
+    int len = strlen(ga__opcodes);//+1
+    for (int i = 0; i < len; i++) ga__memory_tape[i] = ga__opcodes[i];
     //printf("\n После | %03d %03d %03d = %02X %02X %02X", memory_tape[0], memory_tape[1], memory_tape[2], memory_tape[0], memory_tape[1], memory_tape[2]);
 }
 //
 void Debug_Loader_VM()
 {
     //putchar('\n');
-    int len = strlen(gl__memory_tape);//+1
+    int len = strlen(gp__memory_tape);//+1
     printf("\n Оп-код (dec):");
-    for (int i = 0; i < len; i++) printf(" %03d", gl__memory_tape[i]);
+    for (int i = 0; i < len; i++) printf(" %03d", ga__memory_tape[i]);
     printf("\n Оп-код (hex):");
-    for (int i = 0; i < len; i++) printf("  %02X", gl__memory_tape[i]);
+    for (int i = 0; i < len; i++) printf("  %02X", ga__memory_tape[i]);
 }
 //
 void Executor_VM() // Spin / Executor (исполнитель) / Evaluator (древоходец)
 {
     switch_run:
-    switch (*gl__ptr__memory_tape){
+    switch (*gp__memory_tape){
     //
     case 0x75: // 8-bit's addr-on | jmp i8
     {
-        printf("\n \\d%03d = \\h%02X", *gl__ptr__memory_tape, *gl__ptr__memory_tape);
-        //*(++gl__ptr__memory_tape); / Как здесь совершить переход на другой адрес? При помощи computed-goto всё понятно, а вот при помощи switch-case не совсем..
-        //goto switch_run;
-        return;
+        printf("\n \\d%03d = \\h%02X", *gp__memory_tape, *gp__memory_tape);
+        gp__memory_tape = gp__memory_tape + (*(++gp__memory_tape)); // jump offset
+        goto switch_run;
     }
     case 0x01: // 8-bit's addr-on | mov i8, a8
     {
-        printf("\n \\d%03d = \\h%02X", *gl__ptr__memory_tape, *gl__ptr__memory_tape);
-        a8 = *(++gl__ptr__memory_tape);
-        ++gl__ptr__memory_tape;
+        printf("\n \\d%03d = \\h%02X", *gp__memory_tape, *gp__memory_tape);
+        a8 = *(++gp__memory_tape);
+        ++gp__memory_tape;
         goto switch_run;
     }
     case 0x02: // 8-bit's addr-on | mov i8, b8
     {
-        printf("\n \\d%03d = \\h%02X", *gl__ptr__memory_tape, *gl__ptr__memory_tape);
-        b8 = *(++gl__ptr__memory_tape);
-        ++gl__ptr__memory_tape;
+        printf("\n \\d%03d = \\h%02X", *gp__memory_tape, *gp__memory_tape);
+        b8 = *(++gp__memory_tape);
+        ++gp__memory_tape;
         goto switch_run;
     }
     case 0x03: // 8-bit's addr-on | add i8 a8 ; сложение
     {
-        printf("\n \\d%03d = \\h%02X", *gl__ptr__memory_tape, *gl__ptr__memory_tape);
-        a8 += *(++gl__ptr__memory_tape);
-        ++gl__ptr__memory_tape;
+        printf("\n \\d%03d = \\h%02X", *gp__memory_tape, *gp__memory_tape);
+        a8 += *(++gp__memory_tape);
+        ++gp__memory_tape;
         goto switch_run;
     }
     case 0x04: // 8-bit's addr-on | sub i8 a8 ; вычитание
     {
-        printf("\n \\d%03d = \\h%02X", *gl__ptr__memory_tape, *gl__ptr__memory_tape);
-        a8 -= *(++gl__ptr__memory_tape);
-        ++gl__ptr__memory_tape;
+        printf("\n \\d%03d = \\h%02X", *gp__memory_tape, *gp__memory_tape);
+        a8 -= *(++gp__memory_tape);
+        ++gp__memory_tape;
         goto switch_run;
     }
     case 0x05: // 8-bit's addr-on | mul i8 a8 ; умножение
     {
-        printf("\n \\d%03d = \\h%02X", *gl__ptr__memory_tape, *gl__ptr__memory_tape);
-        a8 *= *(++gl__ptr__memory_tape);
-        ++gl__ptr__memory_tape;
+        printf("\n \\d%03d = \\h%02X", *gp__memory_tape, *gp__memory_tape);
+        a8 *= *(++gp__memory_tape);
+        ++gp__memory_tape;
         goto switch_run;
     }
     case 0x06: // 8-bit's addr-on | div i8 a8 ; деление
     {
-        printf("\n \\d%03d = \\h%02X", *gl__ptr__memory_tape, *gl__ptr__memory_tape);
-        a8 /= *(++gl__ptr__memory_tape);
-        ++gl__ptr__memory_tape;
+        printf("\n \\d%03d = \\h%02X", *gp__memory_tape, *gp__memory_tape);
+        a8 /= *(++gp__memory_tape);
+        ++gp__memory_tape;
         goto switch_run;
     }
     case 0x76: // OUT (распечатать строку на консоль)
     {
-        printf("\n \\d%03d = \\h%02X", *gl__ptr__memory_tape, *gl__ptr__memory_tape);
+        printf("\n \\d%03d = \\h%02X", *gp__memory_tape, *gp__memory_tape);
         printf("%s", _rcv8);
-        ++gl__ptr__memory_tape;
+        ++gp__memory_tape;
         goto switch_run;
     }
     case 0x77: // OUT (распечатать число на консоль)
     {
-        printf("\n \\d%03d = \\h%02X", *gl__ptr__memory_tape, *gl__ptr__memory_tape);
-        printf("%d", rcv16);
-        ++gl__ptr__memory_tape;
+        printf("\n \\d%03d = \\h%02X", *gp__memory_tape, *gp__memory_tape);
+        printf("%d", a8);
+        ++gp__memory_tape;
         goto switch_run;
     }
     case 0x78: // OUT (распечатать символ на консоль)
     {
-        printf("\n \\d%03d = \\h%02X", *gl__ptr__memory_tape, *gl__ptr__memory_tape);
-        putchar(rcv16);
-        ++gl__ptr__memory_tape;
+        printf("\n \\d%03d = \\h%02X", *gp__memory_tape, *gp__memory_tape);
+        putchar(a8);
+        ++gp__memory_tape;
         goto switch_run;
     }
     case 0x79: printf("\n Stopped.."); return;
