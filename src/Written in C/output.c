6af0409a9295756c8d91990d6ae736b06ad27ce7
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-25 10:43:48 +1100

    Add files via upload

---
+++ src/Written in C/output.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // Наша спартанская память
+    int x = 0;
+    int y = 0;
+    /***/
+    x = 5;
+    while (x)
+    {
+        y = 10;
+        x--;
+    }
+    /***/
+    return 0;
+}
