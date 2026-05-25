1ebdc2a5d960e92402977a26e72c61d7750cfd5f
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-25 16:14:29 +1100

    Add files via upload

--- src/Written in C/output.c
+++ src/Written in C/output.c
@@ -9,48 +9,21 @@ int main()
     int y = 0;
     int z = 0;
     /***/
-    putchar(4);
+    __main_block_rule();
+}
+
+int rule()
+{
+    y = 10;
+}
+
+int __main_block_rule()
+{
     x = 5;
     while (x)
     {
-        putchar(45);
+        rule();
         x--;
-    }
-    putchar(4);
-    x = 5;
-    y = 53;
-    putchar(10);
-    while (x)
-    {
-        putchar(32);
-        putchar(67);
-        putchar(36);
-        putchar(58);
-        putchar(32);
-        putchar(y);
-        y--;
-        int __flag_0 = x;
-        while (__flag_0)
-        {
-            putchar(10);
-            __flag_0 = 0;
-        }
-        x--;
-    }
-    putchar(4);
-    x = 5;
-    while (x)
-    {
-        putchar(45);
-        x--;
-    }
-    z = 1;
-    int __flag_1 = z;
-    while (__flag_1)
-    {
-        putchar(4);
-        putchar(32);
-        __flag_1 = 0;
     }
     /***/
     return 0;
