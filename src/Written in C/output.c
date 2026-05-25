0dbc7f91df41d9a6d2a7bc6491c0aab68257b83b
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-26 09:38:17 +1100

    Add files via upload

--- src/Written in C/output.c
+++ src/Written in C/output.c
@@ -2,23 +2,18 @@
 #include <locale.h>
 
 // Глобальная спартанская память языка C$
-int x = 0;
-int y = 0;
-int z = 0;
-
-void rule()
-{
-    y = 10;
-}
+int __[100000]; // Единая лента памяти на 100k ячеек
+int x = 0, y = 0, z = 0;
 
 void __main()
 {
-    x = 5;
-    while (x)
+    __[0] = 5;
+    x = __[0];
+    while (x != 0)
     {
-        rule();
-        __main();
+        putchar(65);
         x--;
+        __[0] = x;
     }
 }
 
