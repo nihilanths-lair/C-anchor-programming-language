a245294f739fe7d75f5669a3638329c2dd4a5f19
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-30 09:23:11 +1100

    Add files via upload

--- src/Written in C/c-front.c
+++ src/Written in C/c-front.c
@@ -2,27 +2,25 @@
 #include <windows.h>
 #include <stdint.h>
 
-intptr_t token_type;
-intptr_t token_value;
-intptr_t token_text[64];
-
-void print()
-{
-    printf(" Call: print.");
-}
-
+    token__type_eof = 0;
+    token__while = 1;
+    token__identifier = 2;
+    token__number = 3;
+    token__operator = 4;
+    token__keyword_if = 6;
+    // Ошибка: Ожидался знак '=' 
+    // Ошибка: Ожидался знак '='
 void cdlr__main()
 {
-    putchar('\n');
-    print();
-    putchar('\n');
+    printf("\n Запущен независимый компилятор compiler.cdlr с глобальной ин");
+    // Ошибка синтаксиса
+    // Ошибка: Ожидался знак '=' 
 }
-
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    cdlr__main(); // Вызов главной функции языка C$
+    cdlr__main();
     return 0;
 }
