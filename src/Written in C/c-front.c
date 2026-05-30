4a634735f7b87310db7a1f4bfa96085a1604fa05
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-31 02:15:47 +1100

    Add files via upload

--- src/Written in C/c-front.c
+++ src/Written in C/c-front.c
@@ -1,35 +1,20 @@
-
-
 #include <stdio.h>
-
 #include <windows.h>
-
 #include <stdint.h>
-
-
-
 intptr_t token__eof = 0;
 intptr_t token__keyword_while = 1;
 intptr_t token__identifier = 2;
 intptr_t token__number = 3;
 intptr_t token__operator = 4;
 intptr_t token__keyword_if = 6;
-
 intptr_t source_code = (intptr_t)"// Через эту переменную будем делать самораскрутку компилятора";
 intptr_t position = 0;
-
 intptr_t source_code_2[128];
-
 void cdlr__main()
 {
-
-
     SetConsoleCP(1251);
-
     SetConsoleOutputCP(1251);
-
     
-
     strcpy((char*)source_code_2, "// Исходный код C$ для самораскрутки!");
 
     while (((char*)source_code)[position] != token__eof)
