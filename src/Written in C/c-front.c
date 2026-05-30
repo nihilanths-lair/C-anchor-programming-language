b027d71724bbfd3de50a412fd5df42434ce8fa2f
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-31 00:11:58 +1100

    Add files via upload

--- src/Written in C/c-front.c
+++ src/Written in C/c-front.c
@@ -9,16 +9,25 @@ intptr_t token__number = 3;
 intptr_t token__operator = 4;
 intptr_t token__keyword_if = 6;
 
-intptr_t source_code = (intptr_t)"// Исходный код C$ для самораскрутки!";
+intptr_t source_code = (intptr_t)"// Через эту переменную будем делать самораскрутку компилятора";
 intptr_t position = 0;
+
+intptr_t source_code_2[128];
 
 void cdlr__main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    while (source_code[position] != token__eof)
+
+    strcpy((char*)source_code_2, "// Исходный код C$ для самораскрутки!");
+
+    while (((char*)source_code)[position] != token__eof)
     {
-        printf("\n Текущий символ: '%d'.", source_code[position]);
+        printf("\n Текущий символ (%d): '%c'.", position, ((char*)source_code)[position]);
+        if (((char*)source_code)[position] ==  )
+        {
+            printf(" Обнаружен пробельный символ (%d).")
+        }
         position = position + 1;
     }
     putchar('\n');
