c15d0a115b4118965ad8dcee7e861d6e0f28b33b
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-31 19:02:42 +1100

    Add files via upload

--- src/Written in C/spotlight.c
+++ src/Written in C/spotlight.c
@@ -21,70 +21,67 @@ void Lexer(const char *source_code)
     putchar('\n');
     while (*source_code != '\0')
     {
+        int is_tag_processed = 0; // Флаг, чтобы знать, обрабатывали ли мы тег
         if (*source_code == '<')
         {
-            //printf("%c", *source_code);
             if (!strncmp(source_code, "</emit_c:", strlen("</emit_c:")))
             {
-                source_code += strlen("</emit_c:");
-                // Проверим перенос строки, т.к. у нас строгий (жёсткий) стиль синтаксиса
-                if (*source_code == '\r' && *(source_code+1) == '\n')
+                const char *check_ptr = source_code + strlen("</emit_c:");
+                // Проверим перенос строки для Windows стиль синтаксиса (\r\n)
+                if (*check_ptr == '\r' && *(check_ptr + 1) == '\n')
                 {
-                    source_code += 2; // Перешагиваем
+                    source_code = check_ptr + 2; // Перешагиваем тег и \r\n
                     printf("\n Оператор многострочной вставки С-кода обнаружен.");
-                    while (*source_code != '\0') // Идём по сырому тексту
+                    is_tag_processed = 1;
+                    
+                    while (*source_code != '\0') 
                     {
-                        if (*source_code == '/' && *(source_code+1) == '>') // Выходим из режима записи сырого текста
+                        if (*source_code == '/' && *(source_code + 1) == '>') 
                         {
-                            source_code += 2;
+                            source_code += 1; // Шагаем на '>', а нижний source_code++ выведет нас за тег
                             break;
                         }
-                        else
-                        {
-                            putchar(*source_code);
-                            source_code++;
-                        }
+                        putchar(*source_code);
+                        source_code++;
                     }
                 }
-                else if (*source_code == '\n')
+                // Проверим перенос строки для UNIX стиль синтаксиса (\n)
+                else if (*check_ptr == '\n')
                 {
-                    source_code++; // Перешагиваем
+                    source_code = check_ptr + 1; // Перешагиваем тег и \n
                     printf("\n Оператор многострочной вставки С-кода обнаружен.");
-                    while (*source_code != '\0') // Идём по сырому тексту
+                    is_tag_processed = 1;
+                    
+                    while (*source_code != '\0') 
                     {
-                        if (*source_code == '/' && *(source_code+1) == '>') // Выходим из режима записи сырого текста
+                        if (*source_code == '/' && *(source_code + 1) == '>') 
                         {
-                            source_code += 2;
+                            source_code += 1; // Шагаем на '>', а нижний source_code++ выведет нас за тег
                             break;
                         }
-                        else
-                        {
-                            putchar(*source_code);
-                            source_code++;
-                        }
+                        putchar(*source_code);
+                        source_code++;
                     }
                 }
-                else source_code -= strlen("</emit_c:")-1;
             }
         }
-        else
+        // Если мы только что успешно обработали и закрыли тег, указатель уже сдвинут.
+        // Во всех остальных случаях (обычный символ или ложный <) делаем стандартный шаг вперед.
+        if (is_tag_processed == 0)
         {
-            putchar(*source_code);
+            if (*source_code != '<') { putchar(*source_code); }
             source_code++;
         }
+        else { source_code++; }
     }
 }
 
 int main(int argc, char *argv[])
 {
-    //printf("\n argc: %d", argc);
-    //for (int i = 0; i < argc; i++) printf("\n argv %d: \"%s\"", i+1, argv[i]);
-    //printf("\n Текст на кириллице!");
     printf("\n\n");
-    //printf("\n stitched_source_code\n···\n%s\n···", stitched_source_code);
     Lexer(stitched_source_code);
     printf("\n\n");
-    //for (int i = 0; stitched_source_code[i] != '\0'; i++) printf("%c", stitched_source_code[i]);
+
     if (argc < 2)
     {
         printf("\n compile: %s source_code.cdlr\n", argv[0]);
@@ -99,10 +96,10 @@ int main(int argc, char *argv[])
     fseek(file, 0, SEEK_END);
     long file_size = ftell(file);
     fseek(file, 0, SEEK_SET);
-    //printf("\n\n file_size: %ld\n", file_size);
+    
     fread(source_code, sizeof (char), file_size, file);
     source_code[file_size] = '\0';
-    //printf("\n source_code\n···\n%s\n···", source_code);
+    
     Lexer(source_code);
     putchar('\n');
     return 0;
