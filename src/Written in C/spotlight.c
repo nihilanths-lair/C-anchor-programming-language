48df2233b57653e5b173da31c4aaf06c9a8966e0
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-05-31 19:15:15 +1100

    Add files via upload

--- src/Written in C/spotlight.c
+++ src/Written in C/spotlight.c
@@ -16,60 +16,55 @@ int main(int argc, char *argv[])\r\n\
 />\
 ";
 
-void Lexer(const char *source_code)
+void LexPars(const char *source_code)
 {
-    putchar('\n');
     while (*source_code != '\0')
     {
-        int is_tag_processed = 0; // Флаг, чтобы знать, обрабатывали ли мы тег
+        int is_tag_processed = 0;
         if (*source_code == '<')
         {
             if (!strncmp(source_code, "</emit_c:", strlen("</emit_c:")))
             {
                 const char *check_ptr = source_code + strlen("</emit_c:");
-                // Проверим перенос строки для Windows стиль синтаксиса (\r\n)
                 if (*check_ptr == '\r' && *(check_ptr + 1) == '\n')
                 {
-                    source_code = check_ptr + 2; // Перешагиваем тег и \r\n
-                    printf("\n Оператор многострочной вставки С-кода обнаружен.");
+                    source_code = check_ptr + 2;
+                    // ИСПРАВЛЕНО: fprintf(stderr) выведет текст на экран консоли, и он НЕ попадет в Си-файл!
+                    fprintf(stderr, "\n [Прожектор]: Оператор многострочной вставки С-кода обнаружен.");
                     is_tag_processed = 1;
-                    
-                    while (*source_code != '\0') 
+                    while (*source_code != '\0')
                     {
-                        if (*source_code == '/' && *(source_code + 1) == '>') 
+                        if (*source_code == '/' && *(source_code + 1) == '>')
                         {
-                            source_code += 1; // Шагаем на '>', а нижний source_code++ выведет нас за тег
+                            source_code += 1;
                             break;
                         }
-                        putchar(*source_code);
+                        // ИСПРАВЛЕНО: игнорируем возврат каретки, чтобы Windows не удваивала переносы строк
+                        if (*source_code != '\r') { putchar(*source_code); }
                         source_code++;
                     }
                 }
-                // Проверим перенос строки для UNIX стиль синтаксиса (\n)
                 else if (*check_ptr == '\n')
                 {
-                    source_code = check_ptr + 1; // Перешагиваем тег и \n
-                    printf("\n Оператор многострочной вставки С-кода обнаружен.");
+                    source_code = check_ptr + 1;
+                    fprintf(stderr, "\n [Прожектор]: Оператор многострочной вставки С-кода обнаружен.");
                     is_tag_processed = 1;
-                    
-                    while (*source_code != '\0') 
+                    while (*source_code != '\0')
                     {
-                        if (*source_code == '/' && *(source_code + 1) == '>') 
+                        if (*source_code == '/' && *(source_code + 1) == '>')
                         {
-                            source_code += 1; // Шагаем на '>', а нижний source_code++ выведет нас за тег
+                            source_code += 1;
                             break;
                         }
-                        putchar(*source_code);
+                        if (*source_code != '\r') { putchar(*source_code); }
                         source_code++;
                     }
                 }
             }
         }
-        // Если мы только что успешно обработали и закрыли тег, указатель уже сдвинут.
-        // Во всех остальных случаях (обычный символ или ложный <) делаем стандартный шаг вперед.
         if (is_tag_processed == 0)
         {
-            if (*source_code != '<') { putchar(*source_code); }
+            if (*source_code != '<' && *source_code != '\r') { putchar(*source_code); }
             source_code++;
         }
         else { source_code++; }
@@ -100,7 +95,7 @@ int main(int argc, char *argv[])
     fread(source_code, sizeof (char), file_size, file);
     source_code[file_size] = '\0';
     
-    Lexer(source_code);
+    LexPars(source_code);
     putchar('\n');
     return 0;
 }
