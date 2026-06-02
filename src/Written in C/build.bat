3875fd799ddf97105b881778760c35ed72c0a7d4
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-06-02 15:23:06 +1100

    Add files via upload

--- src/Written in C/build.bat
+++ src/Written in C/build.bat
@@ -2,3 +2,7 @@
 tcc\tcc.exe compiler_stage_0.c -o compiler_stage_0.exe
 ::compiler_stage_0.exe main.meta > nul
 compiler_stage_0.exe main.meta > compiler_stage_1.c
+tcc\tcc.exe compiler_stage_1.c -o compiler_stage_2.exe
+
+:: ИСПРАВЛЕНО: запускаем именно compiler_stage_2.exe, который только что собрали!
+compiler_stage_2.exe main.meta > compiler_stage_2.c
