72a5b4e4ebaca4166c2cfe942e23798f38c2a3a2
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-06-02 19:43:02 +1100

    Add files via upload

--- src/Written in C/build.bat
+++ src/Written in C/build.bat
@@ -1,8 +1,17 @@
 @echo off
-tcc\tcc.exe compiler_stage_0.c -o compiler_stage_0.exe
-::compiler_stage_0.exe main.meta > nul
-compiler_stage_0.exe main.meta > compiler_stage_1.c
-tcc\tcc.exe compiler_stage_1.c -o compiler_stage_2.exe
+:: Стираем старые следы, чтобы убедиться в чистоте сборки
+del compiler_stage_0.exe
+del compiler_stage_1.c
+del compiler_stage_1.exe
+del compiler_stage_2.c
+del compiler_stage_2.exe
 
-:: ИСПРАВЛЕНО: запускаем именно compiler_stage_2.exe, который только что собрали!
-compiler_stage_2.exe main.meta > compiler_stage_2.c
+:: 1. Собираем и запускаем нулевую стадию
+tcc\tcc.exe compiler_stage_0.c -o compiler_stage_0.exe
+compiler_stage_0.exe main.meta > compiler_stage_1.c
+
+:: 2. Собираем первую стадию (наше мета-ядро)
+tcc\tcc.exe compiler_stage_1.c -o compiler_stage_1.exe
+
+:: 3. Запускаем наше мета-ядро НА СЛОВАХ ИЗ ФАЙЛА input.txt!
+compiler_stage_1.exe input.meta > nul
