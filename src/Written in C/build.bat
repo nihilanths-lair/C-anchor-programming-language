491a876f5c09adfac276231fc9e9a0e05fb6dc32
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-06-04 12:16:40 +1100

    Add files via upload

--- src/Written in C/build.bat
+++ src/Written in C/build.bat
@@ -1,19 +1,16 @@
 @echo off
+
 :: Стираем старые следы, чтобы убедиться в чистоте сборки
 del compiler_stage_0.exe
 del compiler_stage_1.c
 del compiler_stage_1.exe
-del compiler_stage_2.c
-del compiler_stage_2.exe
 
-:: 1. Собираем и запускаем нулевую стадию
-tcc\tcc.exe compiler_stage_0.c -o compiler_stage_0.exe
-compiler_stage_0.exe core.meta > compiler_stage_1.c
+:: 1. Слепой Stage 0 копирует чистый Си-код ядра в исходник первой стадии
+compiler_stage_0.exe compiler_stage_0.meta > compiler_stage_1.c
 
-:: 2. Собираем первую стадию (наше мета-ядро)
+:: 2. Собираем бинарник первой стадии
 tcc\tcc.exe compiler_stage_1.c -o compiler_stage_1.exe
 
-:: 3. Запускаем наше мета-ядро НА СЛОВАХ ИЗ ФАЙЛА input.txt!
-::compiler_stage_1.exe bootstrap.meta > nul
-
-compiler_stage_1.exe core.meta > nul
+:: 3. Мета-ядро первой стадии запускает текстовый скрипт bootstrap.meta
+::     и натравливает его на обёрнутый в теги compiler_stage_1.meta!
+compiler_stage_1.exe compiler_stage_1.meta > nul
