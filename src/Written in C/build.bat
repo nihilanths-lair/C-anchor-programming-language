f2ac1174d5c8a8b5a4f0f10447a0bbbe56a7eb24
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-06-15 00:30:08 +1100

    Add files via upload

--- src/Written in C/build.bat
+++ src/Written in C/build.bat
@@ -5,11 +5,11 @@ tcc\tcc.exe stage0\meta_environment.c -o stage0\meta_environment.exe
 :: 2. Переводим ваш красивый синтаксис EASM в промежуточный нативный код .asm
 stage0\meta_environment.exe -c stage0\program.meta stage0\program.bin
 
-:: Указываем FASM, где искать win64ax.inc (Переменная окружения)
-set INCLUDE=fasm\INCLUDE
+:: Указываем FASM точный путь к его родной папке INCLUDE, которая лежит внутри fasm\
+set INCLUDE=fasm\include
 
-:: 3. Натравливаем FASM, который за мгновение соберет полноценный 64-битный .exe
-fasm\FASM.EXE stage0\program.asm stage0\program.exe
+:: 3. Вызываем FASM из его подпапки и собираем 64-битный .exe прямо в папку stage0
+fasm\fasm.exe stage0\program.asm stage0\program.exe
 
 :: 4. ЗАПУСКАЕМ НА СКОРОСТИ КРЕМНИЯ (Без Си-интерпретатора!)
 echo.
