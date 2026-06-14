c3f1d762145060c5259cc39acdaea604e4e89116
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-06-14 19:35:15 +1100

    Add files via upload

--- src/Written in C/build.bat
+++ src/Written in C/build.bat
@@ -1,8 +1,4 @@
-:: 1. Собираем обновленную среду
+@echo off
 tcc\tcc.exe stage0\meta_environment.c -o stage0\meta_environment.exe
-
-:: 2. ПЕРЕКОМПИЛИРУЕМ прошивку заново!
-stage0\meta_environment.exe -c stage0\firmware.meta stage0\firmware.bin
-
-:: 3. Запускаем
-stage0\meta_environment.exe -r stage0\firmware.bin stage0\program.bin
+stage0\meta_environment.exe -c stage0\program.meta stage0\program.bin
+stage0\meta_environment.exe -r stage0\program.bin
