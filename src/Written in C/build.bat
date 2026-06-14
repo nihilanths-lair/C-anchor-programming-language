e6b426e922bc6f67927ab448362f4114b852af7f
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-06-15 00:12:57 +1100

    Add files via upload

--- src/Written in C/build.bat
+++ src/Written in C/build.bat
@@ -1,4 +1,15 @@
 @echo off
+:: 1. Собираем наш Мета-Энвайронмент (Транслятор) через TCC
 tcc\tcc.exe stage0\meta_environment.c -o stage0\meta_environment.exe
+
+:: 2. Переводим ваш красивый синтаксис EASM в промежуточный нативный код .asm
 stage0\meta_environment.exe -c stage0\program.meta stage0\program.bin
-stage0\meta_environment.exe -r stage0\program.bin
+
+:: 3. Натравливаем FASM, который за мгновение соберет полноценный 64-битный .exe
+fasm\fasm.exe stage0\program.asm stage0\program.exe
+
+:: 4. ЗАПУСКАЕМ НА СКОРОСТИ КРЕМНИЯ (Без Си-интерпретатора!)
+echo.
+echo === ЗАПУСК НА РЕАЛЬНОМ ПРОЦЕССОРЕ PC ===
+stage0\program.exe
+echo.
