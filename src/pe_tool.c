82412933c6be0f1e791a600ec605679b47529499
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-07-12 22:42:53 +1100

    Add files via upload

--- src/pe_tool.c
+++ src/pe_tool.c
@@ -554,6 +554,8 @@ void pe_builder(const char * output_filename)
     memcpy(section_header.name, ".text", 5);  // Скопирует 5 символов: '.', 't', 'e', 'x', 't'
     section_header.virtual_address = 4096;    // В памяти секция начнется с RVA 0x1000
     section_header.pointer_to_raw_data = 512; // На диске код начнется со смещения 0x200
+    section_header.virtual_size = 0;          // укажем реальный размер кода (пока 0 байт)
+    section_header.size_of_raw_data = 1024;   // округлим размер на диске по закону диска!
 
     // 4. ПОСЛЕДОВАТЕЛЬНО ЗАПИСЫВАЕМ ВСЁ НА ДИСК
     // Каждая структура улетает монолитным идеальным блоком
