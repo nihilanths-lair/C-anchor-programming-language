6a4a15fa6cf62e8296d218ea772cf88f7a009847
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-07-24 01:58:47 +1100

    Add files via upload

--- src/pe_minimal_tool.c
+++ src/pe_minimal_tool.c
@@ -56,7 +56,7 @@ void pe_minimal_builder(const char * file_name)
     fprintf(file_descriptor, "%c%c", 1, 0);       // 2. Поле NumberOfSections = 1. В LE: сначала 1, затем 0
     fclose(file_descriptor);
 }
-void pe_minimal_analyzer(const char * file_name, uint8_t stream)
+void pe_minimal_analyzer(const char * file_name, FILE * stream)
 {
     FILE * file_descriptor;
     // 1. Открываем файл в бинарном режиме
@@ -74,42 +74,44 @@ void pe_minimal_analyzer(const char * file_name, uint8_t stream)
     // 4. Считываем весь файл в память одним монолитным блоком и закрываем дескриптор
     long bytes_read = fread(file, 1, file_size, file_descriptor); fclose(file_descriptor);
     if (bytes_read != file_size) { printf("\n /!\\: Файл %s не был прочитан полностью", file_name); free(file); return; }
-    printf(" --");
-    printf("\n magic = %u :: %u", // Little-endian :: Big-endian
+    //printf(" Анализ начат.");
+    fprintf(stream, " --");
+    fprintf(stream, "\n magic = %u :: %u", // Little-endian :: Big-endian
      (file[0])      | (file[1] <<  8), // Little-endian (склеиваем байты справа налево, реверсируем)
      (file[0]) << 8 | (file[1]      )  // Big-endian (склеиваем байты слева направо)
     );
-    printf("\n %08llu: %03d | %02X | %c", 0, file[0], file[0], charf(file[0]));
-    printf("\n %08llu: %03d | %02X | %c", 1, file[1], file[1], charf(file[1]));
-    printf("\n --");
-    for (long offset = 2; offset <= 59; offset++) printf("\n %08llu: %03d | %02X | %c", offset, file[offset], file[offset], charf(file[offset]));
-    printf("\n --");
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", 0, file[0], file[0], charf(file[0]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", 1, file[1], file[1], charf(file[1]));
+    fprintf(stream, "\n --");
+    for (long offset = 2; offset <= 59; offset++) fprintf(stream, "\n %08llu: %03d | %02X | %c", offset, file[offset], file[offset], charf(file[offset]));
+    fprintf(stream, "\n --");
     // Читаем lfanew из ПРАВИЛЬНЫХ ячеек (60, 61, 62, 63)
     uint32_t lfanew = (file[60]) | (file[61] << 8) | (file[62] << 16) | (file[63] << 24); // Little-endian (склеиваем байты справа налево, реверсируем)
-    printf("\n lfanew = %u :: %u", lfanew,
+    fprintf(stream, "\n lfanew = %u :: %u", lfanew,
      (file[60]) << 24 | (file[61] << 16) | (file[62] <<  8) | (file[63])  // Big-endian (склеиваем байты слева направо)
     );
-    printf("\n %08llu: %03d | %02X | %c", 60, file[60], file[60], charf(file[60]));
-    printf("\n %08llu: %03d | %02X | %c", 61, file[61], file[61], charf(file[61]));
-    printf("\n %08llu: %03d | %02X | %c", 62, file[62], file[62], charf(file[62]));
-    printf("\n %08llu: %03d | %02X | %c", 63, file[63], file[63], charf(file[63]));
-    printf("\n --");
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", 60, file[60], file[60], charf(file[60]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", 61, file[61], file[61], charf(file[61]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", 62, file[62], file[62], charf(file[62]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", 63, file[63], file[63], charf(file[63]));
+    fprintf(stream, "\n --");
     // --- ЧИТАЕМ СИГНАТУРУ NT_HEADER (Начиная со смещения lfanew) ---
     // Вычисляем смещения для 4 байт сигнатуры
     uint32_t signature = (file[lfanew]) | (file[lfanew+1] << 8) | (file[lfanew+2] << 16) | (file[lfanew+3] << 24); // Little-endian (склеиваем байты справа налево, реверсируем)
-    printf("\n signature = %u :: %u", signature,
+    fprintf(stream, "\n signature = %u :: %u", signature,
      (file[lfanew]) << 24 | (file[lfanew+1] << 16) | (file[lfanew+2] <<  8) | (file[lfanew+3])  // Big-endian (склеиваем байты слева направо)
     );
-    printf("\n %08llu: %03d | %02X | %c", lfanew  , file[lfanew  ], file[lfanew  ], charf(file[lfanew  ]));
-    printf("\n %08llu: %03d | %02X | %c", lfanew+1, file[lfanew+1], file[lfanew+1], charf(file[lfanew+1]));
-    printf("\n %08llu: %03d | %02X | %c", lfanew+2, file[lfanew+2], file[lfanew+2], charf(file[lfanew+2]));
-    printf("\n %08llu: %03d | %02X | %c", lfanew+3, file[lfanew+3], file[lfanew+3], charf(file[lfanew+3]));
-    printf("\n --");
-    uint16_t                        machine = (file[lfanew+4])      | (file[lfanew+5] << 8);
-    printf("\n machine = %u :: %u", machine,  (file[lfanew+4]) << 8 | (file[lfanew+5]    ));
-    printf("\n %08llu: %03d | %02X | %c", lfanew+4, file[lfanew+4], file[lfanew+4], charf(file[lfanew+4]));
-    printf("\n %08llu: %03d | %02X | %c", lfanew+5, file[lfanew+5], file[lfanew+5], charf(file[lfanew+5]));
-    printf("\n --");
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", lfanew  , file[lfanew  ], file[lfanew  ], charf(file[lfanew  ]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", lfanew+1, file[lfanew+1], file[lfanew+1], charf(file[lfanew+1]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", lfanew+2, file[lfanew+2], file[lfanew+2], charf(file[lfanew+2]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", lfanew+3, file[lfanew+3], file[lfanew+3], charf(file[lfanew+3]));
+    fprintf(stream, "\n --");
+    uint16_t                                 machine = (file[lfanew+4])      | (file[lfanew+5] << 8);
+    fprintf(stream, "\n machine = %u :: %u", machine,  (file[lfanew+4]) << 8 | (file[lfanew+5]    ));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", lfanew+4, file[lfanew+4], file[lfanew+4], charf(file[lfanew+4]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", lfanew+5, file[lfanew+5], file[lfanew+5], charf(file[lfanew+5]));
+    fprintf(stream, "\n --");
+    //printf("\n Конец анализа.");
 }
 //#include <locale.h>
 #include <string.h>
@@ -129,24 +131,22 @@ int main(/*int argc, char * argv[]*/)
     fgets(buffer, sizeof (buffer), stdin); // Считывает строку вместе с пробелами (максимум 99 символов + '\0')
     buffer[strcspn(buffer, "\n")] = '\0'; // fgets сохраняет символ переноса строки '\n' в конце, удаляем его, если он мешает
     __start:
-    printf("\n Куда бы вы хотели получить результат:\n В консоль\n В файл\n Оба варианта\n>>> ");
+    printf("\n Куда бы вы хотели получить результат?\n  В консоль\n  В файл\n  Оба варианта\n>>> ");
     fgets(buffer_2, sizeof (buffer_2), stdin); // Считывает строку вместе с пробелами (максимум 99 символов + '\0')
     buffer_2[strcspn(buffer_2, "\n")] = '\0';
     //printf("```\n%s\n```", buffer_2);
-    if (!strcmp(buffer_2, "В консоль"))
+    if (!strcmp(buffer_2, "В консоль")) 
     {
-        printf("\n Анализ начат.");
-        pe_minimal_analyzer(buffer);
-        printf("\n Анализ окончен.\n");
+        pe_minimal_analyzer(buffer, stdout); // Вывод в консоль
+        putchar('\n');
         system("pause");
     }
     else if (!strcmp(buffer_2, "В файл"))
     {
         FILE * file_descriptor = fopen("__.dmp", "wb");
         if (!file_descriptor) return 0;
-        fprintf(file_descriptor, " Анализ начат.");
-        pe_minimal_analyzer(buffer);
-        fprintf(file_descriptor, "\n Анализ окончен.");
+        pe_minimal_analyzer(buffer, file_descriptor); // Вывод в файл
+        fclose(file_descriptor);
         system("pause");
     }
     else
