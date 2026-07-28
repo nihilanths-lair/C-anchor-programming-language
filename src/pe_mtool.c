bafcfcad23a0d157340c1554e1523790ecbac154
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-07-28 15:43:16 +1100

    Add files via upload

--- src/pe_mtool.c
+++ src/pe_mtool.c
@@ -269,13 +269,14 @@ void pe_minimal_analyzer(const char * file_name, FILE * stream)
     offset += 4;
     fprintf(stream, "\n --");
     fprintf(stream, "\n base_of_code = %u :: %u", // (4 байта)
-     (file[lfanew+44]    ) | (file[lfanew+45]<<8 ) | (file[lfanew+46]<<16) | (file[lfanew+47]<<24),
-     (file[lfanew+44]<<24) | (file[lfanew+45]<<16) | (file[lfanew+46]<<8 ) | (file[lfanew+47]    )
+     ((uint32_t) file[offset]    ) | ((uint32_t) file[offset+1]<<8 ) | ((uint32_t) file[offset+2]<<16) | ((uint32_t) file[offset+3]<<24),
+     ((uint32_t) file[offset]<<24) | ((uint32_t) file[offset+1]<<16) | ((uint32_t) file[offset+2]<<8 ) | ((uint32_t) file[offset+3]    )
     );
-    fprintf(stream, "\n %08llu: %03d | %02X | %c", lfanew+44, file[lfanew+44], file[lfanew+44], charf(file[lfanew+44]));
-    fprintf(stream, "\n %08llu: %03d | %02X | %c", lfanew+45, file[lfanew+45], file[lfanew+45], charf(file[lfanew+45]));
-    fprintf(stream, "\n %08llu: %03d | %02X | %c", lfanew+46, file[lfanew+46], file[lfanew+46], charf(file[lfanew+46]));
-    fprintf(stream, "\n %08llu: %03d | %02X | %c", lfanew+47, file[lfanew+47], file[lfanew+47], charf(file[lfanew+47]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", offset  , file[offset  ], file[offset  ], charf(file[offset  ]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", offset+1, file[offset+1], file[offset+1], charf(file[offset+1]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", offset+2, file[offset+2], file[offset+2], charf(file[offset+2]));
+    fprintf(stream, "\n %08llu: %03d | %02X | %c", offset+3, file[offset+3], file[offset+3], charf(file[offset+3]));
+    offset += 4;
     fprintf(stream, "\n --");
     fprintf(stream, "\n image_base = %llu :: %llu", // (8 байт)
      ((uint64_t) file[lfanew+48]    ) | ((uint64_t) file[lfanew+49]<<8 ) | ((uint64_t) file[lfanew+50]<<16) | ((uint64_t) file[lfanew+51]<<24)
